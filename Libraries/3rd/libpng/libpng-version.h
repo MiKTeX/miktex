7e184956200f1fb0e0e0449a755632b07fa43b19
Christian Schenk <cs@miktex.org>
2016-07-24 18:10:00 +0200

    libpng 1.6.23

--- Libraries/3rd/libpng/libpng-version.h
+++ Libraries/3rd/libpng/libpng-version.h
@@ -19,7 +19,7 @@
 
 #define MIKTEX_COMP_J2000_VERSION 6050
 
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.6.21"
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.6.23"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (c) 1998-2016 Glenn Randers-Pehrson"
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "Copyright © 1998-2016 Glenn Randers-Pehrson"
