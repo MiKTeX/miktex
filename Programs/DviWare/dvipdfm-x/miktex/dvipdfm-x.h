d9ef5e334e49920255cea7f0d021bbe3741f1e41
Christian Schenk <cs@miktex.org>
2016-09-10 19:55:17 +0200

    Bug fix: #2527 dvipdfmx does not read all config files (dvipdfmx.cfg)

--- Programs/DviWare/dvipdfm-x/miktex/dvipdfm-x.h
+++ Programs/DviWare/dvipdfm-x/miktex/dvipdfm-x.h
@@ -1,26 +1,30 @@
-/* dvipdfmx-version.h: version number                   -*- C++ -*-
+/* dvipdfm-x/miktex/dvipdfm-x.h:
 
-   Copyright (C) 1996-2016 Christian Schenk
+   Copyright (C) 2016 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
    by the Free Software Foundation; either version 2, or (at your
    option) any later version.
-   
+
    This file is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this file; if not, write to the Free Software
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6097
-#define MIKTEX_COMP_ORIG_VERSION_STR "20160619"
+#pragma once
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1998,1999 Mark Wicks, Copyright (C) 2002-2016 by Jin-Hwan Cho, Matthias Franz, and Shunsaku Hirata, the DVIPDFMx project team"
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1998,1999 Mark Wicks, @ 2002-2016 by Jin-Hwan Cho, Matthias Franz, and Shunsaku Hirata, the DVIPDFMx project team"
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
-#include <miktex/Version>
+void miktex_read_config_files();
+
+#if defined(__cplusplus)
+}
+#endif
