ad6930480149d5acec72b207409d23f106fa970a
Christian Schenk <cs@miktex.org>
2016-10-01 12:01:02 +0200

    RemoteService is a version-dependent value

--- Libraries/MiKTeX/PackageManager/PackageManager.cpp
+++ Libraries/MiKTeX/PackageManager/PackageManager.cpp
@@ -929,9 +929,9 @@ string PackageManagerImpl::GetRemoteServiceBaseUrl()
   if (remoteServiceBaseUrl.empty())
   {
     shared_ptr<Session> session = Session::Get();
-    string section = MIKTEX_REGKEY_PACKAGE_MANAGER + string(".") + std::to_string(MIKTEX_COMP_J2000_VERSION);
     // TODO: use MIKTEX_REGVAL_REMOTE_SERVICE
-    remoteServiceBaseUrl = session->GetConfigValue(section.c_str(), "RemoteService", DEFAULT_REMOTE_SERVICE);
+    string valueName = "RemoteService" + string("_") + std::to_string(MIKTEX_COMP_J2000_VERSION);
+    remoteServiceBaseUrl = session->GetConfigValue(MIKTEX_REGKEY_PACKAGE_MANAGER, valueName.c_str(), DEFAULT_REMOTE_SERVICE);
   }
   return remoteServiceBaseUrl;
 }
