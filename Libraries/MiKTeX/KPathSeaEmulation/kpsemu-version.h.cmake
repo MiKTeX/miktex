6174825c7a7140d976d3baab6fb9db9603250da8
Christian Schenk <cs@miktex.org>
2016-10-07 12:13:59 +0200

    Unify component versioning

--- Libraries/MiKTeX/KPathSeaEmulation/kpsemu-version.h.cmake
+++ Libraries/MiKTeX/KPathSeaEmulation/kpsemu-version.h.cmake
@@ -1,6 +1,6 @@
-/* mpm-version.h: version number                        -*- C++ -*-
+/* kpsemu-version.h: version number			-*- C++ -*-
 
-   Copyright (C) 2001-2016 Christian Schenk
+   Copyright (C) 2000-2016 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,6 +17,4 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6100
-
-#include <miktex/Version>
+#define MIKTEX_COMP_J2000_VERSION ${MIKTEX_COMP_J2000_VERSION}
--- Libraries/MiKTeX/PackageManager/mpm-version.h.cmake
+++ Libraries/MiKTeX/PackageManager/mpm-version.h.cmake
@@ -1,6 +1,6 @@
-/* kpsemu-version.h: version number			-*- C++ -*-
+/* mpm-version.h: version number                        -*- C++ -*-
 
-   Copyright (C) 2000-2016 Christian Schenk
+   Copyright (C) 2001-2016 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,4 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6050
+#define MIKTEX_COMP_J2000_VERSION ${MIKTEX_COMP_J2000_VERSION}
+
+#include <miktex/Version>
