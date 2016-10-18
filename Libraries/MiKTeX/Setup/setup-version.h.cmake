bf8d6a7248ea64bd4e07d8f7001f59ed972718eb
Christian Schenk <cs@miktex.org>
2016-10-18 17:25:45 +0200

    Unify component versioning

--- Libraries/MiKTeX/Setup/setup-version.h.cmake
+++ Libraries/MiKTeX/Setup/setup-version.h.cmake
@@ -17,6 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6100
+#define MIKTEX_COMP_J2000_VERSION ${MIKTEX_COMP_J2000_VERSION}
 
 #include <miktex/Version>
