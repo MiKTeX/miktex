e4d964cafde99c3846445398543644316c093153
Christian Schenk <cs@miktex.org>
2017-01-01 11:49:10 +0100

    freetype2 2.7.1

--- Libraries/3rd/freetype2/freetype2-version.h
+++ Libraries/3rd/freetype2/freetype2-version.h
@@ -1,6 +1,6 @@
 /* freetype2-version.h:                                 -*- C++ -*-
 
-   Copyright (C) 2007-2016 Christian Schenk
+   Copyright (C) 2007-2017 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,9 +17,9 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6200
+#define MIKTEX_COMP_J2000_VERSION 6210
 
-#define MIKTEX_COMP_ORIG_VERSION_STR "2.7"
+#define MIKTEX_COMP_ORIG_VERSION_STR "2.7.1"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 2006-2016 by David Turner, Robert Wilhelm, and Werner Lemberg"
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 2006-2016 by David Turner, Robert Wilhelm, and Werner Lemberg"
