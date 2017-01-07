280cdcfeb7b10ec470e47fb0652a0e0c3d48803f
Christian Schenk <cs@miktex.org>
2017-01-07 11:30:03 +0100

    hunspell 1.6.0

--- Libraries/3rd/hunspell/hunspell-version.h
+++ Libraries/3rd/hunspell/hunspell-version.h
@@ -1,6 +1,6 @@
 /* hunspell-version.h: version number                   -*- C++ -*-
 
-   Copyright (C) 2009-2016 Christian Schenk
+   Copyright (C) 2009-2017 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,9 +17,9 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6200
+#define MIKTEX_COMP_J2000_VERSION 6216
 
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.5.4"
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.6.0"
 
 #define MIKTEX_COMP_COPYRIGHT_STR ""
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 ""
