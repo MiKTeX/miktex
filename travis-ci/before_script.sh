a2d3ec05a49828f7f3748a04746e3f0e3e395875
Christian Schenk <cs@miktex.org>
2017-05-06 21:06:04 +0200

    Travis CI: fix build issues

--- travis-ci/before_script.sh
+++ travis-ci/before_script.sh
@@ -6,9 +6,11 @@ mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
 
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-    vardir=`brew --prefix`/var
-    CMAKE_PREFIX_PATH="`brew --prefix`/opt/icu4c:${CMAKE_PREFIX_PATH}" \
+    brewprefix=`brew --prefix`
+    vardir="${brewprefix}/var
+    CMAKE_PREFIX_PATH="${brewprefix}/opt/icu4c:${brewprefix}/opt/openssl:${CMAKE_PREFIX_PATH}" \
 		     cmake .. \
+		     -DUSE_SYSTEM_MSPACK=FALSE \
 		     -DMIKTEX_MPM_AUTO_ADMIN=t \
 		     -DMIKTEX_MPM_AUTO_INSTALL=t \
 		     -DMIKTEX_SYSTEM_VAR_CACHE_DIR=\"${vardir}/cache\" \
