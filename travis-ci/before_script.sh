67837d7bcdf31f5aa7409966e8727c47dbc170da
Christian Schenk <cs@miktex.org>
2017-05-06 20:57:21 +0200

    Travis CI: set CMAKE_PREFIX_PATH

--- travis-ci/before_script.sh
+++ travis-ci/before_script.sh
@@ -7,9 +7,10 @@ cd "${TRAVIS_BUILD_DIR}/build"
 
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
     vardir=`brew --prefix`/var
-    cmake .. \
-	  -DMIKTEX_MPM_AUTO_ADMIN=t \
-	  -DMIKTEX_MPM_AUTO_INSTALL=t \
-	  -DMIKTEX_SYSTEM_VAR_CACHE_DIR=\"${vardir}/cache\" \
-	  -DMIKTEX_SYSTEM_VAR_LIB_DIR=\"${vardir}/lib\"
+    CMAKE_PREFIX_PATH="`brew --prefix`/opt/icu4c:${CMAKE_PREFIX_PATH}" \
+		     cmake .. \
+		     -DMIKTEX_MPM_AUTO_ADMIN=t \
+		     -DMIKTEX_MPM_AUTO_INSTALL=t \
+		     -DMIKTEX_SYSTEM_VAR_CACHE_DIR=\"${vardir}/cache\" \
+		     -DMIKTEX_SYSTEM_VAR_LIB_DIR=\"${vardir}/lib\"
 fi
