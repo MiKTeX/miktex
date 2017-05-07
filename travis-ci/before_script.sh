6c96f3457b0e912d2b2e2888062cce74b7740313
Christian Schenk <cs@miktex.org>
2017-05-07 19:01:28 +0200

    Travis CI: Ubuntu 14.04

--- travis-ci/before_script.sh
+++ travis-ci/before_script.sh
@@ -5,7 +5,10 @@ set -ev
 mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
 
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+    cmake .. \
+	  -DMIKTEX_MPM_AUTO_INSTALL=t
+elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
     brewprefix=`brew --prefix`
     vardir="${brewprefix}/var"
     CMAKE_PREFIX_PATH="${brewprefix}/opt/icu4c:${brewprefix}/opt/openssl:${CMAKE_PREFIX_PATH}" \
