1d15128e6b5863b7b425799256af0c357cd47708
Christian Schenk <cs@miktex.org>
2017-05-12 15:44:42 +0200

    Run test suite

--- travis-ci/_test.sh
+++ travis-ci/_test.sh
@@ -6,4 +6,12 @@ git clone https://github.com/MiKTeX/miktex-testing
 mkdir test
 cd test
 
-#TODO cmake ..
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+    cmake="${TRAVIS_BUILD_DIR}/mycmake/bin/cmake"
+else
+    cmake=cmake
+fi
+
+${cmake} ..
+
+make test
