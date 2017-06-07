2debb425a4191a6c0dab5914ac74ae18a7906073
Christian Schenk <cs@miktex.org>
2017-06-07 17:34:50 +0200

    deb

--- travis-ci/script.sh
+++ travis-ci/script.sh
@@ -9,8 +9,8 @@ make test
 . "${TRAVIS_BUILD_DIR}/travis-ci/_install.sh"
 . "${TRAVIS_BUILD_DIR}/travis-ci/_test.sh"
 
+cd "${TRAVIS_BUILD_DIR}/build"
 make package
-
 for deb in "${TRAVIS_BUILD_DIR}"/*.deb; do
     curl -sT "${deb}" chunk.io
 done
