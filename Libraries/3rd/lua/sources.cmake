15e7b392be5a0672169e060d2a07092e6fa45b83
Christian Schenk <cs@miktex.org>
2017-06-24 11:49:51 +0200

    Unify component versioning

--- Libraries/3rd/lua/sources.cmake
+++ Libraries/3rd/lua/sources.cmake
@@ -1,5 +1,5 @@
 set(lua_lib_sources
-  ${CMAKE_CURRENT_SOURCE_DIR}/liblua-version.h
+  ${CMAKE_CURRENT_BINARY_DIR}/liblua-version.h
   ${CMAKE_CURRENT_SOURCE_DIR}/source/src/lapi.c
   ${CMAKE_CURRENT_SOURCE_DIR}/source/src/lapi.h
   ${CMAKE_CURRENT_SOURCE_DIR}/source/src/lauxlib.c
