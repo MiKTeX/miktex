3c840b32b493b2d3ce04d6268d6f956929fae1d7
Christian Schenk <cs@miktex.org>
2017-07-06 20:14:29 +0200

    Fix Homebrew build issue

--- travis-ci/before_script.sh
+++ travis-ci/before_script.sh
@@ -21,6 +21,7 @@ elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
 		     -DUSE_SYSTEM_MSPACK=FALSE \
 		     -DUSE_SYSTEM_HARFBUZZ=FALSE \
 		     -DUSE_SYSTEM_HARFBUZZ_ICU=FALSE \
+		     -DMIKTEX_HOMEBREW=TRUE \
 		     -DMIKTEX_MPM_AUTO_ADMIN=t \
 		     -DMIKTEX_MPM_AUTO_INSTALL=t \
 		     -DMIKTEX_SYSTEM_ETC_FONTS_CONFD_DIR="${etcdir}/fonts/conf.d" \
