28cea333b27f04ba057adf11818e87ca184dd4c8
Christian Schenk <cs@miktex.org>
2017-07-16 12:29:27 +0200

    Configuration model

--- Admin/Defaults/mf.defaults.cmake
+++ Admin/Defaults/mf.defaults.cmake
@@ -1,5 +1,5 @@
 ;; Number of autorounded points per cycle.
-max_wiggle=1000
+max_wiggle = 1000
 
 ;; Space for storing moves in a single octant.
-move_size=20000
+move_size = 20000
--- Admin/Defaults/mpost.defaults.cmake
+++ Admin/Defaults/mpost.defaults.cmake
@@ -1,2 +1,2 @@
 ;; Number of words for TFM information for text fonts.
-font_mem_size=10000
+font_mem_size = 10000
