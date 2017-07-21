a05cc1e1f8d8ca94b4e18a47c0f8ae6bb48586c4
Christian Schenk <cs@miktex.org>
2017-07-21 13:54:39 +0200

    Implement Process tests

--- Libraries/MiKTeX/Core/test/process/4.cpp
+++ Libraries/MiKTeX/Core/test/process/4.cpp
@@ -34,10 +34,119 @@ using namespace MiKTeX::Core;
 using namespace MiKTeX::Test;
 using namespace std;
 
+class PipeStream
+{
+public:
+  virtual ~PipeStream()
+  {
+    Close();
+  }
+public:
+  void Open(const MiKTeX::Core::PathName& fileName, const std::vector<std::string>& arguments)
+  {
+    MiKTeX::Core::ProcessStartInfo2 startInfo;
+    startInfo.FileName = fileName.ToString();
+    startInfo.Arguments = arguments;
+    startInfo.RedirectStandardInput = true;
+    startInfo.RedirectStandardError = true;
+    startInfo.RedirectStandardOutput = true;
+    process = MiKTeX::Core::Process::Start(startInfo);
+    inFile = process->get_StandardInput();
+    outFile = process->get_StandardOutput();
+    errFile = process->get_StandardError();
+  }
+public:
+  void Close()
+  {
+    CloseIn();
+    CloseOut();
+    CloseErr();
+    process->WaitForExit(1000);
+  }
+public:
+  PipeStream& operator<<(const std::string& s)
+  {
+    Write(s);
+    return *this;
+  }
+  PipeStream& operator>>(std::string& s)
+  {
+    s = Read();
+    return *this;
+  }
+public:
+  void CloseIn()
+  {
+    if (inFile != nullptr)
+    {
+      fclose(inFile);
+      inFile = nullptr;
+    }
+  }
+protected:
+  void CloseOut()
+  {
+    if (outFile != nullptr)
+    {
+      fclose(outFile);
+      outFile = nullptr;
+    }
+  }
+protected:
+  void CloseErr()
+  {
+    if (errFile != nullptr)
+    {
+      fclose(errFile);
+      errFile = nullptr;
+    }
+  }
+private:
+  FILE* inFile = nullptr;
+private:
+  FILE* outFile = nullptr;
+private:
+  FILE* errFile = nullptr;
+private:
+  void Write(const std::string& s)
+  {
+    if (fwrite(s.c_str(), 1, s.length(), inFile) != s.length())
+    {
+      MIKTEX_FATAL_CRT_ERROR("fwrite");
+    }
+  }
+private:
+  std::string Read()
+  {
+    std::string s;
+    char buf[1024];
+    size_t n = fread(buf, 1, 1024, outFile);
+    if (ferror(outFile) != 0)
+    {
+      MIKTEX_FATAL_CRT_ERROR("fread");
+    }
+    s.assign(buf, n);
+    return s;
+  }
+private:
+  std::unique_ptr<MiKTeX::Core::Process> process;
+};
+
 BEGIN_TEST_SCRIPT("process-4");
 
 BEGIN_TEST_FUNCTION(1);
 {
+#if 1
+  PipeStream pipe;
+  PathName pathExe = pSession->GetMyLocation(false);
+  pathExe /= "core_process_test3-1" MIKTEX_EXE_FILE_SUFFIX;
+  pipe.Open(pathExe, { pathExe.ToString() });
+  pipe << "Hello, world!";
+  pipe.CloseIn();
+  string s;
+  pipe >> s;
+  TEST(s == "Hello, world!");
+#endif
 }
 END_TEST_FUNCTION();
 
