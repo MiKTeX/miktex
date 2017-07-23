d0f26f95a91d65f402a56133adbf98cacd5fab80
Christian Schenk <cs@miktex.org>
2017-07-23 16:05:14 +0200

    Fix build issue

--- Libraries/3rd/fontconfig/config.h
+++ Libraries/3rd/fontconfig/config.h
@@ -18,16 +18,16 @@
 #endif
 
 /* Use libxml2 instead of Expat */
-#undef ENABLE_LIBXML2
+/* #undef ENABLE_LIBXML2 */
 
 /* Additional font directories */
-#undef FC_ADD_FONTS
+/* #undef FC_ADD_FONTS */
 
 /* Architecture prefix to use for cache file names */
-#undef FC_ARCHITECTURE
+/* #undef FC_ARCHITECTURE */
 
 /* System font directory */
-#undef FC_DEFAULT_FONTS
+/* #undef FC_DEFAULT_FONTS */
 
 /* The type of len parameter of the gperf hash/lookup function */
 #define FC_GPERF_SIZE_T unsigned int
@@ -43,22 +43,22 @@
 
 /* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
    */
-#undef HAVE_DIRENT_H
+/* #undef HAVE_DIRENT_H */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+/* #undef HAVE_DLFCN_H */
 
 /* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
-#undef HAVE_DOPRNT
+/* #undef HAVE_DOPRNT */
 
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the `fstatfs' function. */
-#undef HAVE_FSTATFS
+/* #undef HAVE_FSTATFS */
 
 /* Define to 1 if you have the `fstatvfs' function. */
-#undef HAVE_FSTATVFS
+/* #undef HAVE_FSTATVFS */
 
 /* FT_Bitmap_Size structure includes y_ppem field */
 #define HAVE_FT_BITMAP_SIZE_Y_PPEM 1
@@ -73,16 +73,16 @@
 #define HAVE_FT_GET_PS_FONT_INFO 1
 
 /* Define to 1 if you have the `FT_Get_X11_Font_Format' function. */
-#undef HAVE_FT_GET_X11_FONT_FORMAT
+/* #undef HAVE_FT_GET_X11_FONT_FORMAT */
 
 /* Define to 1 if you have the `FT_Has_PS_Glyph_Names' function. */
 #define HAVE_FT_HAS_PS_GLYPH_NAMES 1
 
 /* Define to 1 if you have the `FT_Select_Size' function. */
-#undef HAVE_FT_SELECT_SIZE
+/* #undef HAVE_FT_SELECT_SIZE */
 
 /* Define to 1 if you have the `getexecname' function. */
-#undef HAVE_GETEXECNAME
+/* #undef HAVE_GETEXECNAME */
 
 /* Define to 1 if you have the `getopt' function. */
 #define HAVE_GETOPT 1
@@ -91,126 +91,126 @@
 #define HAVE_GETOPT_LONG 1
 
 /* Define to 1 if you have the `getpagesize' function. */
-#undef HAVE_GETPAGESIZE
+/* #undef HAVE_GETPAGESIZE */
 
 /* Define to 1 if you have the `getprogname' function. */
-#undef HAVE_GETPROGNAME
+/* #undef HAVE_GETPROGNAME */
 
 /* Have Intel __sync_* atomic primitives */
-#undef HAVE_INTEL_ATOMIC_PRIMITIVES
+/* #undef HAVE_INTEL_ATOMIC_PRIMITIVES */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+/* #undef HAVE_INTTYPES_H */
 
 /* Define to 1 if you have the `link' function. */
-#undef HAVE_LINK
+/* #undef HAVE_LINK */
 
 /* Define to 1 if you have the `lrand48' function. */
-#undef HAVE_LRAND48
+/* #undef HAVE_LRAND48 */
 
 /* Define to 1 if you have the `lstat' function. */
-#undef HAVE_LSTAT
+/* #undef HAVE_LSTAT */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+/* #undef HAVE_MEMORY_H */
 
 /* Define to 1 if you have the `mkdtemp' function. */
-#undef HAVE_MKDTEMP
+/* #undef HAVE_MKDTEMP */
 
 /* Define to 1 if you have the `mkostemp' function. */
-#undef HAVE_MKOSTEMP
+/* #undef HAVE_MKOSTEMP */
 
 /* Define to 1 if you have the `mkstemp' function. */
-#undef HAVE_MKSTEMP
+/* #undef HAVE_MKSTEMP */
 
 /* Define to 1 if you have a working `mmap' system call. */
-#undef HAVE_MMAP
+/* #undef HAVE_MMAP */
 
 /* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-#undef HAVE_NDIR_H
+/* #undef HAVE_NDIR_H */
 
 /* Define to 1 if you have the 'posix_fadvise' function. */
-#undef HAVE_POSIX_FADVISE
+/* #undef HAVE_POSIX_FADVISE */
 
 /* Have POSIX threads */
-#undef HAVE_PTHREAD
+/* #undef HAVE_PTHREAD */
 
 /* Have PTHREAD_PRIO_INHERIT. */
-#undef HAVE_PTHREAD_PRIO_INHERIT
+/* #undef HAVE_PTHREAD_PRIO_INHERIT */
 
 /* Define to 1 if you have the `rand' function. */
 #define HAVE_RAND 1
 
 /* Define to 1 if you have the `random' function. */
-#undef HAVE_RANDOM
+/* #undef HAVE_RANDOM */
 
 /* Define to 1 if you have the `random_r' function. */
-#undef HAVE_RANDOM_R
+/* #undef HAVE_RANDOM_R */
 
 /* Define to 1 if you have the `rand_r' function. */
-#undef HAVE_RAND_R
+/* #undef HAVE_RAND_R */
 
 /* Define to 1 if you have the `readlink' function. */
-#undef HAVE_READLINK
+/* #undef HAVE_READLINK */
 
 /* Define to 1 if you have the <sched.h> header file. */
-#undef HAVE_SCHED_H
+/* #undef HAVE_SCHED_H */
 
 /* Have sched_yield */
-#undef HAVE_SCHED_YIELD
+/* #undef HAVE_SCHED_YIELD */
 
 /* Have Solaris __machine_*_barrier and atomic_* operations */
-#undef HAVE_SOLARIS_ATOMIC_OPS
+/* #undef HAVE_SOLARIS_ATOMIC_OPS */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+/* #undef HAVE_STDLIB_H */
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+/* #undef HAVE_STRINGS_H */
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+/* #undef HAVE_STRING_H */
 
 /* Define to 1 if `d_type' is a member of `struct dirent'. */
-#undef HAVE_STRUCT_DIRENT_D_TYPE
+/* #undef HAVE_STRUCT_DIRENT_D_TYPE */
 
 /* Define to 1 if `f_flags' is a member of `struct statfs'. */
-#undef HAVE_STRUCT_STATFS_F_FLAGS
+/* #undef HAVE_STRUCT_STATFS_F_FLAGS */
 
 /* Define to 1 if `f_fstypename' is a member of `struct statfs'. */
-#undef HAVE_STRUCT_STATFS_F_FSTYPENAME
+/* #undef HAVE_STRUCT_STATFS_F_FSTYPENAME */
 
 /* Define to 1 if `f_basetype' is a member of `struct statvfs'. */
-#undef HAVE_STRUCT_STATVFS_F_BASETYPE
+/* #undef HAVE_STRUCT_STATVFS_F_BASETYPE */
 
 /* Define to 1 if `f_fstypename' is a member of `struct statvfs'. */
-#undef HAVE_STRUCT_STATVFS_F_FSTYPENAME
+/* #undef HAVE_STRUCT_STATVFS_F_FSTYPENAME */
 
 /* Define to 1 if `st_mtim' is a member of `struct stat'. */
-#undef HAVE_STRUCT_STAT_ST_MTIM
+/* #undef HAVE_STRUCT_STAT_ST_MTIM */
 
 /* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
    */
-#undef HAVE_SYS_DIR_H
+/* #undef HAVE_SYS_DIR_H */
 
 /* Define to 1 if you have the <sys/mount.h> header file. */
-#undef HAVE_SYS_MOUNT_H
+/* #undef HAVE_SYS_MOUNT_H */
 
 /* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
    */
-#undef HAVE_SYS_NDIR_H
+/* #undef HAVE_SYS_NDIR_H */
 
 /* Define to 1 if you have the <sys/param.h> header file. */
-#undef HAVE_SYS_PARAM_H
+/* #undef HAVE_SYS_PARAM_H */
 
 /* Define to 1 if you have the <sys/statfs.h> header file. */
-#undef HAVE_SYS_STATFS_H
+/* #undef HAVE_SYS_STATFS_H */
 
 /* Define to 1 if you have the <sys/statvfs.h> header file. */
-#undef HAVE_SYS_STATVFS_H
+/* #undef HAVE_SYS_STATVFS_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -219,74 +219,74 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <sys/vfs.h> header file. */
-#undef HAVE_SYS_VFS_H
+/* #undef HAVE_SYS_VFS_H */
 
 /* Define to 1 if `usLowerOpticalPointSize' is a member of `TT_OS2'. */
-#undef HAVE_TT_OS2_USLOWEROPTICALPOINTSIZE
+/* #undef HAVE_TT_OS2_USLOWEROPTICALPOINTSIZE */
 
 /* Define to 1 if `usUpperOpticalPointSize' is a member of `TT_OS2'. */
-#undef HAVE_TT_OS2_USUPPEROPTICALPOINTSIZE
+/* #undef HAVE_TT_OS2_USUPPEROPTICALPOINTSIZE */
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+/* #undef HAVE_UNISTD_H */
 
 /* Define to 1 if you have the `vprintf' function. */
-#undef HAVE_VPRINTF
+/* #undef HAVE_VPRINTF */
 
 /* Can use #warning in C files */
-#undef HAVE_WARNING_CPP_DIRECTIVE
+/* #undef HAVE_WARNING_CPP_DIRECTIVE */
 
 /* Use xmlparse.h instead of expat.h */
-#undef HAVE_XMLPARSE_H
+/* #undef HAVE_XMLPARSE_H */
 
 /* Define to 1 if you have the `XML_SetDoctypeDeclHandler' function. */
-#undef HAVE_XML_SETDOCTYPEDECLHANDLER
+/* #undef HAVE_XML_SETDOCTYPEDECLHANDLER */
 
 /* Define to 1 if you have the `_mktemp_s' function. */
-#undef HAVE__MKTEMP_S
+/* #undef HAVE__MKTEMP_S */
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
+/* #undef LT_OBJDIR */
 
 /* Name of package */
-#undef PACKAGE
+/* #undef PACKAGE */
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+/* #undef PACKAGE_BUGREPORT */
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+/* #undef PACKAGE_NAME */
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+/* #undef PACKAGE_STRING */
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+/* #undef PACKAGE_TARNAME */
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+/* #undef PACKAGE_URL */
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+/* #undef PACKAGE_VERSION */
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
-#undef PTHREAD_CREATE_JOINABLE
+/* #undef PTHREAD_CREATE_JOINABLE */
 
 /* The size of `char', as computed by sizeof. */
-#undef SIZEOF_CHAR
+/* #undef SIZEOF_CHAR */
 
 /* The size of `int', as computed by sizeof. */
-#undef SIZEOF_INT
+/* #undef SIZEOF_INT */
 
 /* The size of `long', as computed by sizeof. */
-#undef SIZEOF_LONG
+/* #undef SIZEOF_LONG */
 
 /* The size of `short', as computed by sizeof. */
-#undef SIZEOF_SHORT
+/* #undef SIZEOF_SHORT */
 
 /* The size of `void*', as computed by sizeof. */
-#undef SIZEOF_VOIDP
+/* #undef SIZEOF_VOIDP */
 
 /* The size of `void *', as computed by sizeof. */
 #if defined(MIKTEX_WINDOWS)
@@ -300,10 +300,10 @@
 #endif
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+/* #undef STDC_HEADERS */
 
 /* Use iconv. */
-#undef USE_ICONV
+/* #undef USE_ICONV */
 
 /* Enable extensions on AIX 3, Interix.  */
 #ifndef _ALL_SOURCE
@@ -328,7 +328,7 @@
 
 
 /* Version number of package */
-#undef VERSION
+/* #undef VERSION */
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
@@ -348,23 +348,23 @@
 #endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
-#undef _FILE_OFFSET_BITS
+/* #undef _FILE_OFFSET_BITS */
 
 /* Define for large files, on AIX-style hosts. */
-#undef _LARGE_FILES
+/* #undef _LARGE_FILES */
 
 /* Define to 1 if on MINIX. */
-#undef _MINIX
+/* #undef _MINIX */
 
 /* Define to 2 if the system does not provide POSIX.1 features except with
    this defined. */
-#undef _POSIX_1_SOURCE
+/* #undef _POSIX_1_SOURCE */
 
 /* Define to 1 if you need to in order for `stat' and other things to work. */
 #undef _POSIX_SOURCE
 
 /* Define to empty if `const' does not conform to ANSI C. */
-#undef const
+/* #undef const */
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
@@ -373,7 +373,7 @@
 #endif
 
 /* Define to `int' if <sys/types.h> does not define. */
-#undef pid_t
+/* #undef pid_t */
 
 #include "config-fixups.h"
 #if defined(MIKTEX) && !defined(DONT_USE_MIKTEX_EXIT)
