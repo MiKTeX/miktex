9b96c5f3b2c12d7198cf077db655c82613b8b88d
Christian Schenk <cs@miktex.org>
2017-08-10 20:08:48 +0200

    Bug fix: #2620 xetex can't input files with BOM anymore

--- Libraries/MiKTeX/TeXAndFriends/inputline.cpp
+++ Libraries/MiKTeX/TeXAndFriends/inputline.cpp
@@ -505,21 +505,24 @@ bool WebAppInputLine::OpenInputFile(FILE** ppFile, const PathName& fileName)
     return false;
   }
 
-  auto openFileInfo = session->TryGetOpenFileInfo(*ppFile);
-  if (openFileInfo.first && openFileInfo.second.mode != FileMode::Command)
+  if (!AmI("xetex"))
   {
-    int bom = CheckBom(*ppFile);
-    switch (bom)
+    auto openFileInfo = session->TryGetOpenFileInfo(*ppFile);
+    if (openFileInfo.first && openFileInfo.second.mode != FileMode::Command)
     {
-    case Bom::UTF8:
-      LogInfo("UTF8 BOM detected: " + openFileInfo.second.fileName);
-      break;
-    case Bom::UTF16be:
-      LogInfo("UTF16be BOM detected: " + openFileInfo.second.fileName);
-      break;
-    case Bom::UTF16le:
-      LogInfo("UTF16le BOM detected: " + openFileInfo.second.fileName);
-      break;
+      int bom = CheckBom(*ppFile);
+      switch (bom)
+      {
+      case Bom::UTF8:
+        LogInfo("UTF8 BOM detected: " + openFileInfo.second.fileName);
+        break;
+      case Bom::UTF16be:
+        LogInfo("UTF16be BOM detected: " + openFileInfo.second.fileName);
+        break;
+      case Bom::UTF16le:
+        LogInfo("UTF16le BOM detected: " + openFileInfo.second.fileName);
+        break;
+      }
     }
   }
 
