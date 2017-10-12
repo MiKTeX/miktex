927a715065693f5d6681f4a014f05e3f933cc722
Christian Schenk <cs@miktex.org>
2017-10-12 16:22:28 +0200

    MetaPost DLL

---
+++ Programs/TeXAndFriends/mplib/component.cmake
@@ -0,0 +1,27 @@
+## component.cmake:
+##
+## Copyright (C) 2017 Christian Schenk
+## 
+## This file is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; either version 2, or (at your
+## option) any later version.
+## 
+## This file is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this file; if not, write to the Free Software
+## Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+## USA.
+
+include(version.cmake)
+
+set(MIKTEX_COMP_DESCRIPTION "MetaPost - a graphics-language processor based on D. E. Knuth's METAFONT")
+
+set(MIKTEX_COMP_COMPANY_STR "")
+set(MIKTEX_COMP_COPYRIGHT_STR "Public domain")
+set(MIKTEX_COMP_COPYRIGHT_STR_1252 "Public domain")
+set(MIKTEX_COMP_TM_STR "TeX is a trademark of the American Mathematical Society. METAFONT is a trademark of Addison-Wesley Publishing Company. PostScript is a trademark of Adobe Systems Incorporated.")
--- Programs/TeXAndFriends/mplib/metapost-version.h.cmake
+++ Programs/TeXAndFriends/mplib/metapost-version.h.cmake
@@ -1,26 +1,29 @@
-/* mpost.rc: version number                             -*- C++ -*-
+/* metapost-version.h: version number                   -*- C++ -*-
 
-   Copyright (C) 2010-2017 Christian Schenk
+   Copyright (C) 2011-2017 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
    by the Free Software Foundation; either version 2, or (at your
    option) any later version.
-
+   
    This file is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this file; if not, write to the Free Software
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-   USA. */
+   USA.  */
 
-#include "mpost-version.h"
+#define MIKTEX_COMP_J2000_VERSION ${MIKTEX_COMP_J2000_VERSION}
 
-#define VER_FILEDESCRIPTION_STR "MetaPost - a graphics-language processor based on D. E. Knuth's METAFONT"
-#define VER_INTERNALNAME_STR "mpost"
-#define VER_ORIGINALFILENAME_STR "miktex-mpost.exe"
+#define MIKTEX_COMP_ORIG_VERSION_STR "${MIKTEX_COMP_ORIG_VERSION_STR}"
 
-#include <miktex/win/version.rc>
+#define MIKTEX_COMP_COMPANY_STR "${MIKTEX_COMP_COMPANY_STR}"
+#define MIKTEX_COMP_COPYRIGHT_STR "${MIKTEX_COMP_COPYRIGHT_STR}"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "${MIKTEX_COMP_COPYRIGHT_STR_1252}"
+#define MIKTEX_COMP_TM_STR "${MIKTEX_COMP_TM_STR}"
+
+#include <miktex/Version>
