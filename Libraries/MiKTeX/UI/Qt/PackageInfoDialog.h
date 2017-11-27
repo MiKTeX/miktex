bbde26960dd5a640f9e5024cd225edd841f8399b
Christian Schenk <cs@miktex.org>
2017-11-27 19:05:23 +0100

    PackageInfoDialog (Qt)

---
+++ Libraries/MiKTeX/UI/Qt/PackageInfoDialog.h
@@ -0,0 +1,43 @@
+/* PackageInfoDialog.h:                                 -*- C++ -*-
+
+   Copyright (C) 2017 Christian Schenk
+
+   This file is part of the MiKTeX UI Library.
+
+   The MiKTeX UI Library is free software; you can redistribute it
+   and/or modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2, or
+   (at your option) any later version.
+
+   The MiKTeX UI Library is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the MiKTeX UI Library; if not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA. */
+
+#ifndef PACKAGEINFODIALOG_H
+#define PACKAGEINFODIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+  class PackageInfoDialog;
+}
+
+class PackageInfoDialog : public QDialog
+{
+  Q_OBJECT
+
+public:
+  explicit PackageInfoDialog(QWidget* parent = nullptr);
+  ~PackageInfoDialog();
+
+private:
+  Ui::PackageInfoDialog* ui;
+};
+
+#endif
---
+++ Libraries/MiKTeX/UI/Qt/include/miktex/UI/Qt/PackageInfoDialog.h
@@ -0,0 +1,58 @@
+/* miktex/UI/Qt/PackageInfoDialog.h:                    -*- C++ -*-
+
+   Copyright (C) 2017 Christian Schenk
+
+   This file is part of MiKTeX UI Library.
+
+   MiKTeX UI Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   MiKTeX UI Library is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MiKTeX UI Library; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+#if defined(_MSC_VER)
+#  pragma once
+#endif
+
+#include "Prototypes"
+
+#include <miktex/PackageManager/PackageManager>
+
+class QWidget;
+
+MIKUI_QT_BEGIN_NAMESPACE;
+
+class MIKTEXNOVTABLE PackageInfoDialog
+{
+public:
+  PackageInfoDialog() = delete;
+
+public:
+  PackageInfoDialog(const PackageInfoDialog& other) = delete;
+
+public:
+  PackageInfoDialog& operator=(const PackageInfoDialog& other) = delete;
+
+public:
+  PackageInfoDialog(PackageInfoDialog&& other) = delete;
+
+public:
+  PackageInfoDialog& operator=(PackageInfoDialog&& other) = delete;
+
+public:
+  ~PackageInfoDialog() = delete;
+
+public:
+  static MIKTEXUIQTCEEAPI(int) DoModal(QWidget* parent, const MiKTeX::Packages::PackageInfo& packageInfo);
+};
+
+MIKUI_QT_END_NAMESPACE;
