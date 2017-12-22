d4fee905e3fc067d757bdabe40a80297fbf7916c
Christian Schenk <cs@miktex.org>
2017-12-22 22:42:35 +0100

    MiKTeX Console: overview

--- Programs/MiKTeX/Console/Qt/mainwindow.cpp
+++ Programs/MiKTeX/Console/Qt/mainwindow.cpp
@@ -1,14 +1,50 @@
+/* mainwindow.cpp:
+
+   Copyright (C) 2017 Christian Schenk
+
+   This file is part of MiKTeX Console.
+
+   MiKTeX Console is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   MiKTeX Console is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MiKTeX Console; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include <miktex/Core/PathName>
+#include <miktex/Core/Session>
+
+using namespace MiKTeX::Core;
+
+MainWindow::MainWindow(QWidget* parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  if (session->IsAdminMode())
+  {
+    ui->userMode->hide();
+  }
+  else
+  {
+    ui->adminMode->hide();
+  }
+  ui->bindir->setText(QString::fromUtf8(session->GetSpecialPath(SpecialPath::BinDirectory).GetData()));
+  ui->installdir->setText(QString::fromUtf8(session->GetSpecialPath(SpecialPath::InstallRoot).GetData()));
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
--- Programs/MiKTeX/Console/Qt/mainwindow.h
+++ Programs/MiKTeX/Console/Qt/mainwindow.h
@@ -1,22 +1,50 @@
+/* mainwindow.h:                                        -*- C++ -*-
+
+   Copyright (C) 2017 Christian Schenk
+
+   This file is part of MiKTeX Console.
+
+   MiKTeX Console is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   MiKTeX Console is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MiKTeX Console; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+
+#include <miktex/Core/Session>
+
+#include <memory>
 
 #include <QMainWindow>
 
 namespace Ui {
-class MainWindow;
+  class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT;
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+  explicit MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow* ui;
+
+private:
+  std::shared_ptr<MiKTeX::Core::Session> session = MiKTeX::Core::Session::Get();
 };
 
-#endif // MAINWINDOW_H
+#endif
