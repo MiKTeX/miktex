f2dd94906682353ffdafe648cfc7b73ec1162aaf
Christian Schenk <cs@miktex.org>
2018-02-24 20:06:21 +0100

    Create links to essential MiKTeX executables (macOS)(#74)

--- Programs/MiKTeX/initexmf/initexmf-version.h
+++ Programs/MiKTeX/initexmf/initexmf-version.h
@@ -17,6 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6608
+#define MIKTEX_COMP_J2000_VERSION 6629
 
 #include <miktex/Version>
--- Programs/MiKTeX/initexmf/initexmf.cpp
+++ Programs/MiKTeX/initexmf/initexmf.cpp
@@ -1640,6 +1640,7 @@ vector<FileLink> miktexFileLinks =
 #if defined(MIKTEX_MACOS_BUNDLE)
   { MIKTEX_INITEXMF_EXE, { MIKTEX_INITEXMF_EXE }},
   { MIKTEX_MPM_EXE, { MIKTEX_MPM_EXE } },
+  { "mthelp" MIKTEX_EXE_FILE_SUFFIX, { "mthelp" MIKTEX_EXE_FILE_SUFFIX } },
 #endif
 #if defined(WITH_MKTEXLSR)
   { MIKTEX_INITEXMF_EXE, { "mktexlsr" }, LinkType::Copy },
