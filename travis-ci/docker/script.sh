545b0cd1de629a4fcec02ed8c745ba23eb60ec8f
Christian Schenk <cs@miktex.org>
2018-05-08 20:00:40 +0200

    Travis CI: use Docker for Linux builds

--- travis-ci/docker/script.sh
+++ travis-ci/docker/script.sh
@@ -18,7 +18,7 @@ git clone https://github.com/MiKTeX/miktex-testing ~/tests/$dist-$tag/test-suite
 docker run -t \
       -v ~/builds/$dist-$tag:/miktex/build:ro \
       -v ~/tests/$dist-$tag/test-suite:/miktex/test-suite:ro \
-      -v ~/teste/$dist-$tag/test:/miktex/test:rw \
+      -v ~/tests/$dist-$tag/test:/miktex/test:rw \
       -e USER_ID=`id -u` \
       -e GROUP_ID=`id -g` \
       miktex/miktex-test-$dist:$tag
