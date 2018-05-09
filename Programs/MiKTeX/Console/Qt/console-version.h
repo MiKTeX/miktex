536517461de3eadcaa276f27f4f7fb6fc48681fc
Christian Schenk <cs@miktex.org>
2018-05-09 12:13:22 +0200

    Switch to admin mode if running as administrator

--- Programs/MiKTeX/Console/Qt/console-version.h
+++ Programs/MiKTeX/Console/Qt/console-version.h
@@ -17,6 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#define MIKTEX_COMP_J2000_VERSION 6701
+#define MIKTEX_COMP_J2000_VERSION 6703
 
 #include <miktex/Version>
