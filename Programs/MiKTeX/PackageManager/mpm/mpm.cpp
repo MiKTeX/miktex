cf4659ed73e404998263bfed9659ab473970d48c
Christian Schenk <cs@miktex.org>
2018-05-09 16:08:42 +0200

    Remove debug output

--- Programs/MiKTeX/PackageManager/mpm/mpm.cpp
+++ Programs/MiKTeX/PackageManager/mpm/mpm.cpp
@@ -1997,8 +1997,6 @@ int MAIN(int argc, MAINCHAR* argv[])
   }
   catch (const MiKTeXException& e)
   {
-    // TODO: remove following line
-    cerr << e.GetSourceFile() << ":" << e.GetSourceLine() << ": " << e.what() << "\n";
     LOG4CXX_FATAL(logger, e.what());
     LOG4CXX_FATAL(logger, "Info: " << e.GetInfo());
     LOG4CXX_FATAL(logger, "Source: " << e.GetSourceFile());
@@ -2008,8 +2006,6 @@ int MAIN(int argc, MAINCHAR* argv[])
   }
   catch (const exception& e)
   {
-    // TODO: remove following line
-    cerr << e.what() << "\n";
     LOG4CXX_FATAL(logger, e.what());
     Sorry();
     retCode = 1;
