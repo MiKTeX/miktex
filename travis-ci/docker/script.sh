737d1ed826c160c8e3fc494301be31e180997a3b
Christian Schenk <cs@miktex.org>
2018-05-10 12:30:01 +0200

    Trave CI: run full tests only when tagged or started via cron job

--- travis-ci/docker/script.sh
+++ travis-ci/docker/script.sh
@@ -5,7 +5,18 @@ set -v
 
 dist=$1
 tag=$2
+runTests=$3
 
+if [ -z $runTests ]; then
+    runTests=false
+    if [ ! -z $TRAVIS_TAG ]; then
+	runTests=true
+    fi
+    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+	runTests=true
+    fi
+fi
+   
 mkdir -p ~/builds/$dist-$tag
 docker run -t \
       -v "${TRAVIS_BUILD_DIR}:/miktex/source:ro" \
@@ -13,12 +24,14 @@ docker run -t \
       -e USER_ID=`id -u` \
       -e GROUP_ID=`id -g` \
       miktex/miktex-build-$dist:$tag
-mkdir -p ~/tests/$dist-$tag/test
-git clone https://github.com/MiKTeX/miktex-testing ~/tests/$dist-$tag/test-suite
-docker run -t \
+if [ "$runTests" == "true" ]; then
+    mkdir -p ~/tests/$dist-$tag/test
+    git clone https://github.com/MiKTeX/miktex-testing ~/tests/$dist-$tag/test-suite
+    docker run -t \
       -v ~/builds/$dist-$tag:/miktex/build:ro \
       -v ~/tests/$dist-$tag/test-suite:/miktex/test-suite:ro \
       -v ~/tests/$dist-$tag/test:/miktex/test:rw \
       -e USER_ID=`id -u` \
       -e GROUP_ID=`id -g` \
       miktex/miktex-test-$dist:$tag
+fi
--- travis-ci/osx/script.sh
+++ travis-ci/osx/script.sh
@@ -3,9 +3,23 @@
 set -e
 set -v
 
+runTests=$1
+
+if [ -z $runTests ]; then
+    runTests=false
+    if [ ! -z $TRAVIS_TAG ]; then
+	runTests=true
+    fi
+    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+	runTests=true
+    fi
+fi
+
 cd "${TRAVIS_BUILD_DIR}/build"
 make
 make test
 
-. "${TRAVIS_BUILD_DIR}/travis-ci/osx/_install.sh"
-. "${TRAVIS_BUILD_DIR}/travis-ci/osx/_test.sh"
+if [ "$runTests" == "true" ]; then
+    . "${TRAVIS_BUILD_DIR}/travis-ci/osx/_install.sh"
+    . "${TRAVIS_BUILD_DIR}/travis-ci/osx/_test.sh"
+fi
