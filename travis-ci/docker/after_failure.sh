c94b4d1e9e0a21c1fde2cad4a42ebb1476bebebe
Christian Schenk <cs@miktex.org>
2018-06-03 12:19:28 +0200

    Travis CI: cat log files

--- travis-ci/docker/after_failure.sh
+++ travis-ci/docker/after_failure.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-set -v
-
 dist=$MIKTEX_LINUX_DIST
 tag=$MIKTEX_LINUX_DIST_CODE_NAME
 
 if [ -f "~/tests/$dist-$tag/test/Testing/Temporary/LastTest.log" ]; then
     cat "~/tests/$dist-$tag/test/Testing/Temporary/LastTest.log"
+else
+    echo log file does not exist: "~/tests/$dist-$tag/test/Testing/Temporary/LastTest.log"
 fi
 
 if [ -d "~/tests/$dist-$tag/test/logfiles" ]; then
     (cd "~/tests/$dist-$tag/test/logfiles"; cat *)
+else
+    echo log directory does not exist: "~/tests/$dist-$tag/test/logfiles"
 fi
