61b36cdf191e6b07431057f5710833d7c3a92099
Christian Schenk <christian.schenk@mitel.com>
2018-09-05 16:43:46 +0200

    Fix formatting

--- Programs/MiKTeX/Console/Qt/console-version.h
+++ Programs/MiKTeX/Console/Qt/console-version.h
@@ -17,6 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#define MIKTEX_COMP_J2000_VERSION 6809
+#define MIKTEX_COMP_J2000_VERSION 6821
 
 #include <miktex/Version>
