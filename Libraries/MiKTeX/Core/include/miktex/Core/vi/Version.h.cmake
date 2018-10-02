ee5dd93bdb6c5652867373ff42403f475b8a9a19
Christian Schenk <cs@miktex.org>
2018-10-02 10:55:32 +0200

    Use #pragma once

--- Libraries/MiKTeX/Core/include/miktex/Core/vi/Version.h.cmake
+++ Libraries/MiKTeX/Core/include/miktex/Core/vi/Version.h.cmake
@@ -19,9 +19,7 @@
    Software Foundation, 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA. */
 
-#if defined(_MSC_VER)
-#  pragma once
-#endif
+#pragma once
 
 #if !defined(A55138B37E4B4CF4ADA0BB63BCD95D97)
 #define A55138B37E4B4CF4ADA0BB63BCD95D97
