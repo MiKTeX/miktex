77d375857e6049cb715918c74e161c1122d1b583
Christian Schenk <cs@miktex.org>
2018-10-04 13:32:39 +0200

    log4cxx: UTF-8 logging (Windows)

--- Libraries/3rd/log4cxx/log4cxx-version.h
+++ Libraries/3rd/log4cxx/log4cxx-version.h
@@ -1,6 +1,6 @@
-/* apr-version.h: version number                        -*- C++ -*-
+/* log4cxx-version.h: version number                    -*- C++ -*-
 
-   Copyright (C) 2015-2016 Christian Schenk
+   Copyright (C) 2015-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6300
+#define MIKTEX_COMP_J2000_VERSION 6851
 
 #define MIKTEX_COMP_ORIG_VERSION_STR "0.10.0"
 
