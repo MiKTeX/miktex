1991a809b0e608e172a277b7420ccafc6e4a7353
Christian Schenk <cs@miktex.org>
2018-10-05 20:43:08 +0200

    expat 2.2.6

--- Libraries/3rd/expat/expat-version.h
+++ Libraries/3rd/expat/expat-version.h
@@ -1,6 +1,6 @@
 /* expat-version.h: version number                      -*- C++ -*-
 
-   Copyright (C) 1996-2016 Christian Schenk
+   Copyright (C) 1996-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,8 +17,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6300
-#define MIKTEX_COMP_ORIG_VERSION_STR "2.2.0"
+#define MIKTEX_COMP_J2000_VERSION 6852
+#define MIKTEX_COMP_ORIG_VERSION_STR "2.2.6"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (c) 1998-2000 Thai Open Source Software Center Ltd, (c) 2001-2016 Expat maintainers"
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1998-2000 Thai Open Source Software Center Ltd, © 2001-2016 Expat maintainers"
