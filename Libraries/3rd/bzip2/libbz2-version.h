6c94d111cc17c2afca21cb9b7945c6e7e14b0113
Christian Schenk <cs@miktex.org>
2018-10-05 16:35:37 +0200

    Cosmetics

--- Libraries/3rd/bzip2/libbz2-version.h
+++ Libraries/3rd/bzip2/libbz2-version.h
@@ -1,6 +1,6 @@
-/* libbz2-version.h: version number			-*- C++ -*-
+/* libbz2-version.h: version number                     -*- C++ -*-
 
-   Copyright (C) 1996-2016 Christian Schenk
+   Copyright (C) 1996-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6300
+#define MIKTEX_COMP_J2000_VERSION 6852
 
 #define MIKTEX_COMP_ORIG_VERSION_STR "1.0.6"
 
