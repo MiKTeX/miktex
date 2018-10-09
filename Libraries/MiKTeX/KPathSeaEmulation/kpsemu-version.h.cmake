84bcaabb654b1d60afe0c0f9b7c1395a08aba80e
Christian Schenk <cs@miktex.org>
2018-10-09 19:29:55 +0200

    Cosmetics

--- Libraries/MiKTeX/KPathSeaEmulation/kpsemu-version.h.cmake
+++ Libraries/MiKTeX/KPathSeaEmulation/kpsemu-version.h.cmake
@@ -1,6 +1,6 @@
-/* kpsemu-version.h: version number			-*- C++ -*-
+/* kpsemu-version.h: version number                     -*- C++ -*-
 
-   Copyright (C) 2000-2016 Christian Schenk
+   Copyright (C) 2000-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
