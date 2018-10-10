2436c8afea1bfdc571b7f1d08967c9ed58aa036f
Christian Schenk <christian.schenk@mitel.com>
2018-10-10 13:33:26 +0200

    Use #pragma once

--- Libraries/MiKTeX/Setup/include/miktex/Setup/SetupService.h
+++ Libraries/MiKTeX/Setup/include/miktex/Setup/SetupService.h
@@ -17,9 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#if defined(_MSC_VER)
-#  pragma once
-#endif
+#pragma once
 
 #if !defined(FAA6EA3649584988994D2B7032C040AB)
 #define FAA6EA3649584988994D2B7032C040AB
