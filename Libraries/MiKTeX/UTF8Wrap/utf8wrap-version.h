de4d51befc3026212715382ae69bdfbd44a34dbe
Christian Schenk <christian.schenk@mitel.com>
2018-10-11 17:03:02 +0200

    Cosmetics

--- Libraries/MiKTeX/UTF8Wrap/utf8wrap-version.h
+++ Libraries/MiKTeX/UTF8Wrap/utf8wrap-version.h
@@ -1,6 +1,6 @@
-/* utf8wrap-version.h: version number			-*- C++ -*-
+/* utf8wrap-version.h: version number                   -*- C++ -*-
 
-   Copyright (C) 2011-2016 Christian Schenk
+   Copyright (C) 2011-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
