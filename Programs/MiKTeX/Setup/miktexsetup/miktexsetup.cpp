e665b1ada37f3eae69a97486fd27bbda604c3e27
Christian Schenk <christian.schenk@mitel.com>
2018-10-12 16:36:26 +0200

    Fire StdAfx.h

--- Programs/MiKTeX/Setup/miktexsetup/miktexsetup.cpp
+++ Programs/MiKTeX/Setup/miktexsetup/miktexsetup.cpp
@@ -17,7 +17,59 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#include "StdAfx.h"
+#include <climits>
+#include <cstdio>
+
+#include <stdarg.h>
+
+#if defined(_MSC_VER)
+#  pragma warning (push, 1)
+#  pragma warning (disable: 4702)
+#endif
+
+#if defined(HAVE_CONFIG_H)
+#  include <config.h>
+#endif
+
+#if defined(_WIN32)
+#  include <Windows.h>
+#endif
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+#include <vector>
+
+#include <signal.h>
+
+#if defined(_MSC_VER)
+#  pragma warning (pop)
+#endif
+
+#include "miktexsetup-version.h"
+
+#include <miktex/Core/Exceptions>
+#include <miktex/Core/Paths>
+#include <miktex/Core/Quoter>
+#include <miktex/Core/Session>
+#include <miktex/PackageManager/PackageManager>
+#include <miktex/Setup/SetupService>
+#include <miktex/Trace/Trace>
+#include <miktex/Util/StringUtil>
+#include <miktex/Wrappers/PoptWrapper>
+
+#if defined(MIKTEX_WINDOWS)
+#  include <MiKTeX/Core/Help>
+#endif
+
+using namespace MiKTeX::Core;
+using namespace MiKTeX::Packages;
+using namespace MiKTeX::Setup;
+using namespace MiKTeX::Util;
+using namespace MiKTeX::Wrappers;
+using namespace std;
 
 #include "internal.h"
 
