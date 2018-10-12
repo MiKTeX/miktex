aef74da225cad71e638d2d57d8cb4c4f9cb3bdc5
Christian Schenk <christian.schenk@mitel.com>
2018-10-12 14:18:08 +0200

    Cosmetics

--- Programs/FontUtilities/ttf2pk2/ttf2pk-version.h
+++ Programs/FontUtilities/ttf2pk2/ttf2pk-version.h
@@ -1,6 +1,6 @@
-/* ttf2pk-version.h: version number			-*- C++ -*-
+/* ttf2pk-version.h: version number                     -*- C++ -*-
 
-   Copyright (C) 1996-2016 Christian Schenk
+   Copyright (C) 1996-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
