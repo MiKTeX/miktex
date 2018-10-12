91ab99eb1d8c7f1a1b3c0fc49324cd4985a025c4
Christian Schenk <christian.schenk@mitel.com>
2018-10-12 15:26:16 +0200

    Use #pragma once

--- Programs/TeXAndFriends/pdftex/pdftex-miktex.h
+++ Programs/TeXAndFriends/pdftex/pdftex-miktex.h
@@ -17,9 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#if defined(HAVE_PRAGMA_ONCE)
-#  pragma once
-#endif
+#pragma once
 
 #if !defined(B9AE601D55FC414A8D93C81CF3517D1A)
 #define B9AE601D55FC414A8D93C81CF3517D1A
