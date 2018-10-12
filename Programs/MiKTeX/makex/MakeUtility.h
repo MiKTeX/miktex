84f5a617c74cb577e623fb6c443034f26a271517
Christian Schenk <christian.schenk@mitel.com>
2018-10-12 15:56:38 +0200

    Use #pragma once

--- Programs/MiKTeX/makex/MakeUtility.h
+++ Programs/MiKTeX/makex/MakeUtility.h
@@ -17,9 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#if defined(_MSC_VER)
-#  pragma once
-#endif
+#pragma once
 
 #if defined(_MSC_VER)
 #  pragma warning (disable: 4786)
