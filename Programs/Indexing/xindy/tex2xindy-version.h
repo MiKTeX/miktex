7e7a44ffe3787635a1f62f5dfcec0ee85fdb6d4f
Christian Schenk <christian.schenk@mitel.com>
2018-10-12 14:32:07 +0200

    Cosmetics

--- Programs/Indexing/xindy/tex2xindy-version.h
+++ Programs/Indexing/xindy/tex2xindy-version.h
@@ -1,6 +1,6 @@
-/* tex2xindy-version.h: version number			-*- C++ -*-
+/* tex2xindy-version.h: version number                  -*- C++ -*-
 
-   Copyright (C) 2015-2016 Christian Schenk
+   Copyright (C) 2015-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
