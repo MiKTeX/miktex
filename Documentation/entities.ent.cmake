04976ae03eb9b7c7707023324166b03283e97b96
Christian Schenk <cs@miktex.org>
2018-10-13 15:26:52 +0200

    Remove obsolete stuff

--- Documentation/entities.ent.cmake
+++ Documentation/entities.ent.cmake
@@ -1,67 +1,43 @@
-<!ENTITY BSD "<acronym>BSD</acronym>">
 <!ENTITY BasicMiKTeXInstaller "<application>Basic MiKTeX Installer</application>">
 <!ENTITY BibTeX "<application>BibTeX</application>">
-<!ENTITY CD "<acronym>CD</acronym>">
 <!ENTITY CTAN "<acronym>CTAN</acronym>">
-<!ENTITY CommonConfig "<varname>CommonConfig</varname>">
-<!ENTITY CommonData "<varname>CommonData</varname>">
-<!ENTITY CommonInstall "<varname>CommonInstall</varname>">
-<!ENTITY Cygwin "<application>Cygwin</application>">
-<!ENTITY DVD "<acronym>DVD</acronym>">
-<!ENTITY DVDdashR "<acronym>DVD-R</acronym>">
 <!ENTITY DVI "<acronym>DVI</acronym>">
-<!ENTITY DebugView "<application>DebugView</application>">
-<!ENTITY DonaldEKnuth "Donald&nbsp;E.&nbsp;Knuth">
 <!ENTITY Dvipdfm "<application>Dvipdfm</application>">
 <!ENTITY Dvips "<application>Dvips</application>">
 <!ENTITY EPS "<acronym>EPS</acronym>">
 <!ENTITY EXESUFFIX "${EXESUFFIX}">
 <!ENTITY FAQ "<acronym>FAQ</acronym>">
 <!ENTITY FNDB "<acronym>FNDB</acronym>">
-<!ENTITY FSF "<acronym>FSF</acronym>">
-<!ENTITY FTP "<acronym>FTP</acronym>">
 <!ENTITY GF "<acronym>GF</acronym>">
-<!ENTITY GNU "<acronym>GNU</acronym>">
 <!ENTITY GPG "<acronym>GPG</acronym>">
-<!ENTITY GPL "<acronym>GPL</acronym>">
 <!ENTITY Ghostscript "<application>Ghostscript</application>">
 <!ENTITY HTML "<acronym>HTML</acronym>">
 <!ENTITY HTTP "<acronym>HTTP</acronym>">
-<!ENTITY Homebrew "<application>Homebrew</application>">
 <!ENTITY JBIG2 "<acronym>JBIG2</acronym>">
 <!ENTITY JPG "<acronym>JPG</acronym>">
-<!ENTITY LPPL "<acronym>LPPL</acronym>">
 <!ENTITY LW35 "<acronym>LW35</acronym>">
 <!ENTITY LaTeX "<application>LaTeX</application>">
 <!ENTITY Linux "<application>Linux</application>">
 <!ENTITY Lua "<application>Lua</application>">
 <!ENTITY LuaTeX "<application>LuaTeX</application>">
-<!ENTITY MD5Walk "<application>MD5Walk</application>">
 <!ENTITY METAFONT "<application>METAFONT</application>">
 <!ENTITY MIKTEX_PREFIX "${MIKTEX_PREFIX}">
 <!ENTITY MLTeX "<application>MLTeX</application>">
-<!ENTITY MPC "<acronym>MPC</acronym>">
 <!ENTITY MPM "<acronym>MPM</acronym>">
 <!ENTITY MakeIndex "<application>MakeIndex</application>">
 <!ENTITY MetaPost "<application>MetaPost</application>">
 <!ENTITY MiKTeX "<application>MiKTeX</application>">
 <!ENTITY MiKTeXConsole "<application>&MiKTeX; Console</application>">
 <!ENTITY MiKTeXConfigurationUtility "&MiKTeX; Configuration Utility">
-<!ENTITY MiKTeXCurrentSeries "&MiKTeX;&nbsp;${MIKTEX_MAJOR_MINOR_STR}">
 <!ENTITY MiKTeXHelpUtility "<application>&MiKTeX; Help Utility</application>">
 <!ENTITY MiKTeXNetInstaller "<application>MiKTeX Net Installer</application>">
-<!ENTITY MiKTeXOptions "<application>&MiKTeX; Options</application>">
-<!ENTITY MiKTeXPackageCreator "<application>&MiKTeX; Package Creator</application>">
 <!ENTITY MiKTeXPackageManager "<application>&MiKTeX; Package Manager</application>">
 <!ENTITY MiKTeXPortable "<application>&MiKTeX;&nbsp;Portable</application>">
 <!ENTITY MiKTeXPrintUtility "<application>&MiKTeX; Print Utility</application>">
 <!ENTITY MiKTeXSetupUtility "<application>&MiKTeX; Setup Utility</application>">
 <!ENTITY MiKTeXSetupWizard "<application>&MiKTeX; Setup Wizard</application>">
-<!ENTITY MiKTeXUpdateWizard "<application>&MiKTeX; Update Wizard</application>">
 <!ENTITY MiKTeXconfigurationdatastore "&MiKTeX; configuration data store">
 <!ENTITY MiKTeXpdfTeX "<application>MiKTeX-pdfTeX</application>">
-<!ENTITY OSXElCapitan "<application>OS&nbsp;X&nbsp;El&nbsp;Capitan</application>">
-<!ENTITY OSXYosemite "<application>OS&nbsp;X&nbsp;Yosemite</application>">
 <!ENTITY Omega "<application>Omega</application>">
 <!ENTITY PACKAGE_NAME "${PACKAGE_NAME}">
 <!ENTITY PDF "<acronym>PDF</acronym>">
@@ -70,50 +46,26 @@
 <!ENTITY PostScript "<application>PostScript</application>">
 <!ENTITY SVG "<acronym>SVG</acronym>">
 <!ENTITY SyncTeX "<application>SyncTeX</application>">
-<!ENTITY TCX "<acronym>TCX</acronym>">
 <!ENTITY TeXLive "<application>TeX Live</application>">
 <!ENTITY TDS "<acronym>TDS</acronym>">
 <!ENTITY TEXMF "<acronym>TEXMF</acronym>">
 <!ENTITY TeX "<application>TeX</application>">
 <!ENTITY TeXAndFriends "<application>TeX</application>&nbsp;&amp;&nbsp;Friends">
 <!ENTITY TeXMF "&TeX;MF">
-<!ENTITY TeXify "<application>TeXify</application>">
 <!ENTITY TeXnFriends "&TeX;&nbsp;&amp;&nbsp;Friends">
 <!ENTITY TeXworks "<application>TeXworks</application>">
-<!ENTITY Terminal "<application>Terminal</application>">
 <!ENTITY Texinfo "<application>Texinfo</application>">
 <!ENTITY URL "<acronym>URL</acronym>">
 <!ENTITY URW "<acronym>URW</acronym>">
 <!ENTITY UTC "<acronym>UTC</acronym>">
-<!ENTITY Ubuntu "<application>Ubuntu</application>">
-<!ENTITY UserConfig "<varname>UserConfig</varname>">
-<!ENTITY UserData "<varname>UserData</varname>">
-<!ENTITY UserInstall "<varname>UserInstall</varname>">
 <!ENTITY Windows "<application>Windows</application>">
-<!ENTITY Windows10 "<application>Windows&nbsp;10</application>">
-<!ENTITY Windows7 "<application>Windows&nbsp;7</application>">
-<!ENTITY Windows8 "<application>Windows&nbsp;8</application>">
-<!ENTITY Windows81 "<application>Windows&nbsp;8.1</application>">
 <!ENTITY WindowsRegistry "<application>Windows Registry</application>">
-<!ENTITY WindowsServer2003R2 "<application>Windows Server&nbsp;2003&nbsp;R2</application>">
-<!ENTITY WindowsServer2003ServicePack2 "<application>Windows Server&nbsp;2003 with Service Pack&nbsp;2</application>">
-<!ENTITY WindowsServer2008R2 "<application>Windows Server&nbsp;2008&nbsp;R2</application>">
-<!ENTITY WindowsServer2008ServicePack2 "<application>Windows Server&nbsp;2008 with Service Pack&nbsp;2</application>">
-<!ENTITY WindowsServer2012R2 "<application>Windows Server&nbsp;2012&nbsp;R2</application>">
-<!ENTITY WindowsSysinternals "<application>Windows Sysinternals</application>">
-<!ENTITY WindowsVistaServicePack2 "<application>Windows&nbsp;Vista with Service Pack&nbsp;2</application>">
-<!ENTITY WindowsXPServicePack3 "<application>Windows&nbsp;XP with Service Pack&nbsp;3</application>">
 <!ENTITY XDV "<acronym>XDV</acronym>">
 <!ENTITY XeTeX "<application>XeTeX</application>">
 <!ENTITY Yap "<application>Yap</application>">
-<!ENTITY aptget "<command>apt-get</command>">
 <!ENTITY basicmiktexexe "<command>basic-miktex-${MIKTEX_MAJOR_MINOR_STR}.<replaceable>xxxx</replaceable>${EXESUFFIX}</command>">
-<!ENTITY basicmiktexexefile "<filename>basic-miktex-${MIKTEX_MAJOR_MINOR_STR}.<replaceable>xxxx</replaceable>${EXESUFFIX}</filename>">
-<!ENTITY brew "<command>brew</command>">
-<!ENTITY cabextract "<command>cabextract</command>">
 <!ENTITY commoninstalldir "<filename>${COMMONINSTALL}</filename>">
 <!ENTITY date "${MIKTEX_CACHED_MONTH_NAME} ${MIKTEX_CACHED_MDAY}, ${MIKTEX_CACHED_YEAR}">
-<!ENTITY docbaseurl "${MIKTEX_DOC_BASE_URL}">
 <!ENTITY eTeX "<application>eTeX</application>">
 <!ENTITY encTeX "<application>encTeX</application>">
 <!ENTITY findtexmf "<command>findtexmf</command>">
@@ -122,17 +74,10 @@
 <!ENTITY inimf "<command>inimf</command>">
 <!ENTITY initex "<command>initex</command>">
 <!ENTITY initexmf "<command>initexmf</command>">
-<!ENTITY latex "<command>latex</command>">
-<!ENTITY lzma "<command>lzma</command>">
 <!ENTITY macOS "<application>macOS</application>">
-<!ENTITY macOSSierra "<application>macOS&nbsp;Sierra</application>">
 <!ENTITY makeinfo "<command>makeinfo</command>">
-<!ENTITY md5walk "<command>md5walk</command>">
-<!ENTITY memoir "<application>memoir</application>">
 <!ENTITY miktexdvicopy "<command>${MIKTEX_PREFIX}dvicopy</command>">
-<!ENTITY miktexepstopdf "<command>${MIKTEX_PREFIX}epstopdf</command>">
 <!ENTITY miktexgftodvi "<command>${MIKTEX_PREFIX}gftodvi</command>">
-<!ENTITY miktexlatex "<command>${MIKTEX_PREFIX}latex</command>">
 <!ENTITY miktexmf "<command>${MIKTEX_PREFIX}mf</command>">
 <!ENTITY miktexmpost "<command>${MIKTEX_PREFIX}mpost</command>">
 <!ENTITY miktexpdflatex "<command>${MIKTEX_PREFIX}pdflatex</command>">
@@ -142,8 +87,6 @@
 <!ENTITY miktexsetup "<command>miktexsetup</command>">
 <!ENTITY miktextex "<command>${MIKTEX_PREFIX}tex</command>">
 <!ENTITY miktexxetex "<command>${MIKTEX_PREFIX}xetex</command>">
-<!ENTITY month "${MIKTEX_CACHED_MONTH_NAME} ${MIKTEX_CACHED_YEAR}">
-<!ENTITY mpc "<command>mpc</command>">
 <!ENTITY mpm "<command>mpm</command>">
 <!ENTITY mthelp "<command>mthelp</command>">
 <!ENTITY mtprint "<command>mtprint</command>">
@@ -151,16 +94,9 @@
 <!ENTITY ndash "<symbol>&#8211;</symbol>">
 <!ENTITY pTeX "<application>pTeX</application>">
 <!ENTITY pdfTeX "<application>pdfTeX</application>">
-<!ENTITY progfldr "MiKTeX ${MIKTEX_MAJOR_MINOR_STR}">
 <!ENTITY prompt "${PROMPT}">
-<!ENTITY promptunx "${PROMPTUNX}">
-<!ENTITY promptwin "${PROMPTWIN}">
 <!ENTITY rev "${MIKTEX_MAJOR_MINOR_STR}.${MIKTEX_COMP_J2000_VERSION}">
-<!ENTITY rsync "<command>rsync</command>">
 <!ENTITY setupexe "<command>setup-${MIKTEX_MAJOR_MINOR_STR}.<replaceable>xxxx</replaceable>${EXESUFFIX}</command>">
-<!ENTITY setupexefile "<filename>setup-${MIKTEX_MAJOR_MINOR_STR}.<replaceable>xxxx</replaceable>${EXESUFFIX}</filename>">
-<!ENTITY tar "<command>tar</command>">
-<!ENTITY texi2dvi "<command>tex2dvi</command>">
 <!ENTITY texify "<command>texify</command>">
 <!ENTITY userdatadir "<filename>${USERDATA}</filename>">
 <!ENTITY userinstalldir "<filename>${USERINSTALL}</filename>">
