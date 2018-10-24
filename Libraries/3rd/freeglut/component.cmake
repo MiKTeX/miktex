be2b7f4c5009e042f1260791b3ca3c1c7b585f99
Christian Schenk <christian.schenk@mitel.com>
2018-10-24 11:58:10 +0200

    Cosmetics

--- Libraries/3rd/freeglut/component.cmake
+++ Libraries/3rd/freeglut/component.cmake
@@ -19,7 +19,7 @@
 
 include(version.cmake)
 
-set(MIKTEX_COMP_DESCRIPTION " Freeglut - the Free OpenGL Utility Toolkit")
+set(MIKTEX_COMP_DESCRIPTION "Freeglut - the Free OpenGL Utility Toolkit")
 
 set(MIKTEX_COMP_COMPANY_STR "")
 set(MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1999-2000 Pawel W. Olszta. All Rights Reserved.")
