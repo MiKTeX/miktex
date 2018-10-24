c82b29db7033a58351b5fe21a14fdb0913efd883
Christian Schenk <christian.schenk@mitel.com>
2018-10-24 14:33:32 +0200

    Cosmetics

--- Libraries/3rd/regex/regex-version.h
+++ Libraries/3rd/regex/regex-version.h
@@ -1,6 +1,6 @@
-/* regex-version.h: version number			-*- C++ -*-
+/* regex-version.h: version number                      -*- C++ -*-
 
-   Copyright (C) 1996-2015Christian Schenk
+   Copyright (C) 1996-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
