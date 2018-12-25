d9fc311c290ef563d93645d7d21dd105357873d3
Christian Schenk <cs@miktex.org>
2018-12-25 21:45:15 +0100

    TL sync

--- Libraries/MiKTeX/KPathSeaEmulation/test/kpsewhich/kpsewhich-version.h
+++ Libraries/MiKTeX/KPathSeaEmulation/test/kpsewhich/kpsewhich-version.h
@@ -1,6 +1,6 @@
 /* makeindex-version.h: version number                  -*- C++ -*-
 
-   Copyright (C) 2010-2016 Christian Schenk
+   Copyright (C) 2010-2018 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,11 +17,11 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 6300
+#define MIKTEX_COMP_J2000_VERSION 6933
 
-#define MIKTEX_COMP_ORIG_VERSION_STR "6.0.0"
+#define MIKTEX_COMP_ORIG_VERSION_STR "6.3.1/dev"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright 2010 Karl Berry & Olaf Weber"
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 2010 Karl Berry & Olaf Weber"
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright 2018 Karl Berry & Olaf Weber"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 2018 Karl Berry & Olaf Weber"
 
 #include <miktex/Version>
