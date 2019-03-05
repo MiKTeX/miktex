5b4f6f635123f8e3ab88e550e11625717a5667ed
Christian Schenk <cs@miktex.org>
2019-03-05 21:02:52 +0100

    Documentation

--- Libraries/MiKTeX/PackageManager/PackageDataStore.h
+++ Libraries/MiKTeX/PackageManager/PackageDataStore.h
@@ -1,6 +1,6 @@
 /* PackageDataStore.h:                                  -*- C++ -*-
 
-   Copyright (C) 2018 Christian Schenk
+   Copyright (C) 2018-2019 Christian Schenk
 
    This file is part of MiKTeX Package Manager.
 
@@ -43,41 +43,84 @@
 
 BEGIN_INTERNAL_NAMESPACE;
 
+/// @brief An instance of this class collects package records.
+///
+/// The record data is retrieved from two sources:
+/// - `miktex/config/package-manifests.ini`: immutable package manifests
+/// - `miktex/config/packages.ini`: mutable package data such as installation timestamps
 class PackageDataStore
 {
 public:
   PackageDataStore();
 
+  /// @brief Load package records into the data store.
+  ///
+  /// This method loads
+  /// - mutable data from a given INI file
+  /// - immutable package data from `miktex/config/packages.ini`
+  /// 
+  /// In addition, package dependencies and file reference counts are
+  /// calculated.
+  ///
+  /// @param path Path to the INI file.
 public:
-  void LoadAllPackageManifests(const MiKTeX::Core::PathName& packageManifestsPath);
+  void LoadAllPackageManifests(const MiKTeX::Core::PathName& path);
 
+  /// Save mutable package data.
 public:
   void SaveVarData();
 
+  /// Clear the contents of the data store.
 public:
   void Clear();
 
+  /// Try to retrieve a record from the data store.
+  /// @param packageId The package ID.
+  /// @return The first return value indicates whether the record (second return value) could be retrieved.
 public:
   std::tuple<bool, MiKTeX::Packages::PackageInfo> TryGetPackage(const std::string& packageId);
 
+  /// Retrieve a record from the data store.
+  /// @exception std::exception Record not found.
+  /// @param packageId The package ID.
+  /// @return Returns the requested record.
 public:
   MiKTeX::Packages::PackageInfo GetPackage(const std::string& packageId)
   {
     return (*this)[packageId];
   }
 
+  /// Add a record to the data store.
+  /// @param packageInfo The record to be added.
+public:
+  void DefinePackage(const MiKTeX::Packages::PackageInfo& packageinfo);
+
+  /// Update a record in the data store.
+  /// @param packageInfo The record to update.
 public:
   void SetPackage(const MiKTeX::Packages::PackageInfo& packageInfo)
   {
     (*this)[packageInfo.id] = packageInfo;
   }
 
+  /// @brief Set the package installation timestamp.
+  ///
+  /// If the timestamp is zero, the mutable package data will cleared
+  /// (the package is not installed).
+  /// 
+  /// @param packageId The package ID.
+  /// @param timeInstalled The installation timestamp.
 public:
   void SetTimeInstalled(const std::string& packageId, std::time_t timeInstalled);
 
+  /// Declare a package as obsolete.
+  /// @param packageId The package ID.
 public:
   void DeclareObsolete(const std::string& packageId);
 
+  /// Set the release state of an installed package.
+  /// @param packageId The package ID.
+  /// @param releaseState The release state.
 public:
   void SetReleaseState(const std::string& packageId, MiKTeX::Packages::RepositoryReleaseState releaseState);
 
@@ -117,24 +160,37 @@ public:
     PackageDefinitionTable::iterator it;
   };
 
+  /// Get an iterator to the first record.
 public:
   iterator begin();
 
+  /// Get an iterator referring to the past-the-end record.
 public:
   iterator end();
 
-public:
-  void DefinePackage(const MiKTeX::Packages::PackageInfo& packageinfo);
-
+  /// Increment the reference counts of all files in a package.
+  /// @param path The package ID.
 public:
   void IncrementFileRefCounts(const std::string& packageId);
 
+  /// Get the reference count of a file.
+  /// @param path The path to the file.
+  /// @return Returns reference count of the file.
 public:
   unsigned long GetFileRefCount(const MiKTeX::Core::PathName& path);
 
+  /// Decrement the reference count of an installed file.
+  /// @param path The path to the file.
+  /// @return Returns the new (decremented) reference count.
 public:
   unsigned long DecrementFileRefCount(const MiKTeX::Core::PathName& path);
 
+  /// Migrate TPM files into a single INI file.
+  ///
+  /// If the INI file `miktex/config/package-manifests.ini` does not
+  /// yet exists, it will be created by parsing all TPM files. This
+  /// method can be used to migrate an old (pre 2019) package database
+  /// into the new format.
 public:
   void NeedPackageManifestsIni();
 
