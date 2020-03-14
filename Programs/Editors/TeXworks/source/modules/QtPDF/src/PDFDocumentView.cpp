f5f8b432a60a4300b038d360a9096407bd1a3424
Christian Schenk <cs@miktex.org>
2020-03-14 17:52:07 +0100

    TeXworks sync

--- Programs/Editors/TeXworks/source/modules/QtPDF/src/PDFDocumentView.cpp
+++ Programs/Editors/TeXworks/source/modules/QtPDF/src/PDFDocumentView.cpp
@@ -2335,25 +2335,33 @@ void PDFPageGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsIte
   
     int i, imin, imax;
     int j, jmin, jmax;
-  
-    imin = (visibleRect.left() - pageRect.left()) / TILE_SIZE;
+    // Each tile is rendered at TILE_SIZE pixels, which may be scaled (e.g. on
+    // high-dpi screens) and displayed at an effective size
+    int effectiveTileSize = TILE_SIZE / painter->device()->devicePixelRatio();
+
+    imin = (visibleRect.left() - pageRect.left()) / effectiveTileSize;
     imax = (visibleRect.right() - pageRect.left());
-    if (imax % TILE_SIZE == 0)
-      imax /= TILE_SIZE;
+    if (imax % effectiveTileSize == 0)
+      imax /= effectiveTileSize;
     else
-      imax = imax / TILE_SIZE + 1;
+      imax = imax / effectiveTileSize + 1;
   
-    jmin = (visibleRect.top() - pageRect.top()) / TILE_SIZE;
+    jmin = (visibleRect.top() - pageRect.top()) / effectiveTileSize;
     jmax = (visibleRect.bottom() - pageRect.top());
-    if (jmax % TILE_SIZE == 0)
-      jmax /= TILE_SIZE;
+    if (jmax % effectiveTileSize == 0)
+      jmax /= effectiveTileSize;
     else
-      jmax = jmax / TILE_SIZE + 1;
+      jmax = jmax / effectiveTileSize + 1;
   
     for (j = jmin; j < jmax; ++j) {
       for (i = imin; i < imax; ++i) {
-        QRect tile(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-  
+        // renderTile is the rect used for rendering/retrieving tiles. It is
+        // agnostic of the painter (e.g., its devicePixelRatio)
+        QRect renderTile(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        // displayTile is the rect used for displaying. It takes the painter's
+        // settings into account (e.g. its devicePixelRatio)
+        QRect displayTile(i * effectiveTileSize, j * effectiveTileSize, effectiveTileSize, effectiveTileSize);
+
         bool useGrayScale = false;
         // If we are rendering a PDFDocumentView that has `useGrayScale` set
         // respect that setting.
@@ -2367,7 +2375,7 @@ void PDFPageGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsIte
             useGrayScale = true;
         }
 
-        renderedPage = page->getTileImage(this, _dpiX * scaleFactor, _dpiY * scaleFactor, tile);
+        renderedPage = page->getTileImage(this, _dpiX * scaleFactor * painter->device()->devicePixelRatio(), _dpiY * scaleFactor * painter->device()->devicePixelRatio(), renderTile);
         // we don't want a finished render thread to change our image while we
         // draw it
         page->document()->pageCache().lock();
@@ -2378,14 +2386,18 @@ void PDFPageGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsIte
             // page image to avoid altering the cached (color) image
             QImage postProcessed = renderedPage->copy();
             imageToGrayScale(postProcessed);
-            painter->drawImage(tile.topLeft(), postProcessed);
+            postProcessed.setDevicePixelRatio(painter->device()->devicePixelRatio());
+            painter->drawImage(displayTile.topLeft(), postProcessed);
           }
-          else
-            painter->drawImage(tile.topLeft(), *renderedPage);
+          else {
+            QImage img = *renderedPage;
+            img.setDevicePixelRatio(painter->device()->devicePixelRatio());
+            painter->drawImage(displayTile.topLeft(), img);
+          }
         }
         page->document()->pageCache().unlock();
 #ifdef DEBUG
-        painter->drawRect(tile);
+        painter->drawRect(displayTile);
 #endif
       }
     }
