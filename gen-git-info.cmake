8abac1d3592d3b3fb84ad0fd4ff726f50e99b0b2
Christian Schenk <cs@miktex.org>
2020-03-22 09:20:55 +0100

    Git info

--- gen-git-info.cmake
+++ gen-git-info.cmake
@@ -16,29 +16,38 @@
 ## along with this file; if not, write to the Free Software Foundation,
 ## 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-if(GIT_FOUND)
+if("$Format:%%$" STREQUAL "%%")
   set(MIKTEX_HAVE_GIT_INFO 1)
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%H
-    WORKING_DIRECTORY "${SOURCE_DIR}"
-    OUTPUT_VARIABLE MIKTEX_GIT_COMMIT
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%h
-    WORKING_DIRECTORY "${SOURCE_DIR}"
-    OUTPUT_VARIABLE MIKTEX_GIT_COMMIT_ABBREV
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%at
-    WORKING_DIRECTORY "${SOURCE_DIR}"
-    OUTPUT_VARIABLE MIKTEX_GIT_AUTHOR_DATE
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+  set(MIKTEX_GIT_COMMIT "$Format:%H$")
+  set(MIKTEX_GIT_COMMIT_ABBREV "$Format:%h$")
+  set(MIKTEX_GIT_AUTHOR_DATE $Format:%at$)
+elseif(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/.git")
+  if(GIT_FOUND)
+    set(MIKTEX_HAVE_GIT_INFO 1)
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%H
+      WORKING_DIRECTORY "${SOURCE_DIR}"
+      OUTPUT_VARIABLE MIKTEX_GIT_COMMIT
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%h
+      WORKING_DIRECTORY "${SOURCE_DIR}"
+      OUTPUT_VARIABLE MIKTEX_GIT_COMMIT_ABBREV
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} show --no-patch --pretty=%at
+      WORKING_DIRECTORY "${SOURCE_DIR}"
+      OUTPUT_VARIABLE MIKTEX_GIT_AUTHOR_DATE
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  else()
+    set(MIKTEX_HAVE_GIT_INFO 0)
+  endif()
 else()
   set(MIKTEX_HAVE_GIT_INFO 0)
 endif()
