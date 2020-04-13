34418396f904f392982a37bd5ddbc0d9890f26a5
Christian Schenk <cs@miktex.org>
2020-04-13 21:00:18 +0200

    Trace level

--- Libraries/MiKTeX/App/app.cpp
+++ Libraries/MiKTeX/App/app.cpp
@@ -67,6 +67,7 @@ using namespace MiKTeX::Trace;
 using namespace MiKTeX::Util;
 
 static log4cxx::LoggerPtr logger;
+static bool isLog4cxxConfigured = false;
 
 static Application* instance = nullptr;
 
@@ -129,8 +130,6 @@ public:
 public:
   shared_ptr<Session> session;
 public:
-  bool isLog4cxxConfigured = false;
-public:
   string commandLine;
 };
 
@@ -153,7 +152,6 @@ Application::~Application() noexcept
     {
       Finalize();
     }
-    FlushPendingTraceMessages();
   }
   catch (const exception&)
   {
@@ -273,12 +271,12 @@ void Application::ConfigureLogging()
     Utils::SetEnvironmentString("MIKTEX_LOG_DIR", logDir.ToString());
     Utils::SetEnvironmentString("MIKTEX_LOG_NAME", logName);
     log4cxx::xml::DOMConfigurator::configure(xmlFileName.ToWideCharString());
-    pimpl->isLog4cxxConfigured = true;
   }
   else
   {
     log4cxx::BasicConfigurator::configure();
   }
+  isLog4cxxConfigured = true;
   logger = log4cxx::Logger::getLogger(myName);
 }
 
@@ -430,7 +428,7 @@ void Application::AutoDiagnose()
 
   for (const Setup::Issue& issue : issues)
   {
-    if (pimpl->isLog4cxxConfigured)
+    if (isLog4cxxConfigured)
     {
       if (issue.severity == Setup::IssueSeverity::Critical || issue.severity == Setup::IssueSeverity::Major)
       {
@@ -479,7 +477,7 @@ void Application::Init(const Session::InitInfo& initInfoArg)
   }
   else
   {
-    LOG4CXX_INFO(logger, "started #" << thisProcess->GetSystemId() << " by " << invokerName << " with command line: " << pimpl->commandLine);
+    LOG4CXX_INFO(logger, "this process (" << thisProcess->GetSystemId() << ") started by " << invokerName << " with command line: " << pimpl->commandLine);
   }
   pimpl->beQuiet = false;
   if (pimpl->enableInstaller == TriState::Undetermined)
@@ -540,17 +538,18 @@ void Application::Finalize2(int exitCode)
   if (logger != nullptr)
   {
     auto thisProcess = Process::GetCurrentProcess();
-    LOG4CXX_INFO(logger, "#" << thisProcess->GetSystemId() << " finishes with exit code " << exitCode);
+    LOG4CXX_INFO(logger, "this process (" << thisProcess->GetSystemId() << ") finishes with exit code " << exitCode);
   }
   Finalize();
 }
-  
+
 void Application::Finalize()
 {
   if (pimpl->enableDiagnose == TriState::True)
   {
     AutoDiagnose();
   }
+  FlushPendingTraceMessages();
   if (pimpl->installer != nullptr)
   {
     pimpl->installer->Dispose();
@@ -569,15 +568,16 @@ void Application::Finalize()
   }
   logger = nullptr;
   pimpl->initialized = false;
+  instance = nullptr;
 }
 
 void Application::ReportLine(const string& str)
 {
+  MIKTEX_ASSERT(logger != nullptr);
   LOG4CXX_INFO(logger, "mpm: " << str);
   if (!GetQuietFlag())
   {
-    fputs(str.c_str(), stdout);
-    putc('\n', stdout);
+    cout << str << endl;
   }
 }
 
@@ -801,7 +801,7 @@ TriState Application::GetEnableInstaller() const
 
 void Application::Trace(const TraceCallback::TraceMessage& traceMessage)
 {
-  if (!pimpl->isLog4cxxConfigured)
+  if (!isLog4cxxConfigured)
   {
     if (pimpl->pendingTraceMessages.size() > 100)
     {
@@ -825,7 +825,7 @@ void Application::FlushPendingTraceMessages()
 
 void Application::TraceInternal(const TraceCallback::TraceMessage& traceMessage)
 {
-  if (pimpl->isLog4cxxConfigured)
+  if (isLog4cxxConfigured)
   {
     log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger(string("trace.") + Utils::GetExeName() + "." + traceMessage.facility);
     switch (traceMessage.level)
@@ -880,10 +880,11 @@ void Application::Sorry(const string& name, const string& description, const str
         << "\n"
         << T_("Remedy:") << "\n"
         << "\n"
-        << "  " << remedy << endl;
+        << "  " << remedy
+        << endl;
     }
   }
-  if (instance != nullptr && instance->pimpl->isLog4cxxConfigured)
+  if (isLog4cxxConfigured)
   {
     log4cxx::RollingFileAppenderPtr appender = log4cxx::Logger::getRootLogger()->getAppender(LOG4CXX_STR("RollingLogFile"));
     if (appender != nullptr)
@@ -892,14 +893,16 @@ void Application::Sorry(const string& name, const string& description, const str
         << "\n"
         << "The log file hopefully contains the information to get MiKTeX going again:" << "\n"
         << "\n"
-        << "  " << PathName(appender->getFile()) << endl;
+        << "  " << PathName(appender->getFile())
+        << endl;
     }
   }
   if (!url.empty())
   {
     cerr
       << "\n"
-      << T_("For more information, visit:") << " " << url << endl;
+      << T_("For more information, visit:") << " " << url
+      << endl;
   }
 }
 
@@ -928,7 +931,8 @@ void Application::Sorry(const string& name, const exception& ex)
   else
   {
     cerr
-      << "ERROR: " << ex.what() << "\n";
+      << "ERROR: " << ex.what()
+      << endl;
   }
   Sorry(name);
 }
@@ -1090,7 +1094,7 @@ shared_ptr<Session> Application::GetSession() const
 
 void Application::LogInfo(const std::string& message) const
 {
-  if (pimpl->isLog4cxxConfigured)
+  if (logger != nullptr)
   {
     LOG4CXX_INFO(logger, message);
   }
@@ -1098,7 +1102,7 @@ void Application::LogInfo(const std::string& message) const
 
 void Application::LogWarn(const std::string& message) const
 {
-  if (pimpl->isLog4cxxConfigured)
+  if (logger != nullptr)
   {
     LOG4CXX_WARN(logger, message);
   }
@@ -1106,7 +1110,7 @@ void Application::LogWarn(const std::string& message) const
 
 void Application::LogError(const std::string& message) const
 {
-  if (pimpl->isLog4cxxConfigured)
+  if (logger != nullptr)
   {
     LOG4CXX_ERROR(logger, message);
   }
