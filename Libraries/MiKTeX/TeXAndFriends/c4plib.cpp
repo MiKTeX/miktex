6dea242ffb16744f0ca45025e458354c26b45343
Christian Schenk <cs@miktex.org>
2020-06-01 12:32:19 +0200

    Fix Windows build

--- Libraries/MiKTeX/TeXAndFriends/c4plib.cpp
+++ Libraries/MiKTeX/TeXAndFriends/c4plib.cpp
@@ -19,7 +19,7 @@
    Software Foundation, 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA. */
 
-#if defined(MIKTEX_WINDPWS)
+#if defined(MIKTEX_WINDOWS)
 #include <io.h>
 #else
 #include <unistd.h>
