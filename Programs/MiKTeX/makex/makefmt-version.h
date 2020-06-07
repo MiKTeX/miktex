0880ace09e0f0acfd870c0381653038ea97870ba
Christian Schenk <cs@miktex.org>
2020-06-07 21:06:35 +0200

    New versioning scheme

--- Programs/MiKTeX/makex/makefmt-version.h
+++ Programs/MiKTeX/makex/makefmt-version.h
@@ -19,8 +19,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#define MIKTEX_COMP_MAJOR_VERSION 2
-#define MIKTEX_COMP_MINOR_VERSION 10
+#define MIKTEX_COMP_MAJOR_VERSION 4
+#define MIKTEX_COMP_MINOR_VERSION 0
 #define MIKTEX_COMP_PATCH_VERSION 0
 
 #include <miktex/Version>
