8217b00558ff7de26f3a38f904c6c59437696d3a
Christian Schenk <cs@miktex.org>
2021-12-23 22:35:28 +0000

    miktex fontmaps

---
+++ Programs/MiKTeX/miktex/topics/fontmaps/commands/commands.h
@@ -0,0 +1,28 @@
+/* topics/fontmaps/commands/commands.h:
+
+   Copyright (C) 2021 Christian Schenk
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2, or (at your
+   option) any later version.
+   
+   This file is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this file; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA.  */
+
+#include <string>
+#include <vector>
+
+#include "internal.h"
+
+namespace OneMiKTeXUtility::Topics::FontMaps::Commands
+{
+    int Update(OneMiKTeXUtility::ApplicationContext& ctx, const std::vector<std::string>& arguments);
+}
---
+++ Programs/MiKTeX/miktex/topics/fontmaps/topic.cpp
@@ -0,0 +1,89 @@
+/* topics/fontmaps/topic.cpp:
+
+   Copyright (C) 2021 Christian Schenk
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2, or (at your
+   option) any later version.
+   
+   This file is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this file; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA.  */
+
+#include <iostream>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <miktex/Core/Text>
+
+#include "commands/commands.h"
+#include "internal.h"
+#include "topic.h"
+
+using namespace std;
+
+using namespace OneMiKTeXUtility;
+using namespace OneMiKTeXUtility::Topics;
+using namespace OneMiKTeXUtility::Topics::FontMaps;
+
+#define T_(x) MIKTEXTEXT(x)
+
+class FontMapsTopic :
+    public Topic
+{
+private:
+    std::string Description() override
+    {
+        return T_("Commands for managing TeX font map files.");
+    }
+
+private:
+    int MIKTEXTHISCALL Execute(ApplicationContext& ctx, const std::vector<std::string>& arguments) override;
+
+private:
+    std::string Name() override
+    {
+        return "fontmaps";
+    }
+
+private:
+    string BadUsage(const std::string s)
+    {
+        return fmt::format(T_("bad fontmaps usage: {0}"), s);
+    }
+};
+
+unique_ptr<Topic> OneMiKTeXUtility::Topics::FontMaps::Create()
+{
+    return make_unique<FontMapsTopic>();
+}
+
+int FontMapsTopic::Execute(ApplicationContext& ctx, const vector<string>& arguments)
+{
+    if (arguments.size() < 2)
+    {
+        ctx.ui->Error(BadUsage(T_("missing command; try help")));
+        return 1;
+    }
+    if (arguments[1] == "help")
+    {
+        ctx.ui->Output("fontmaps commands:");
+        ctx.ui->Output("  help         show help");
+        ctx.ui->Output("  update       update TeX font map files");
+        return 0;
+    }
+    if (arguments[1] == "update")
+    {
+        return Commands::Update(ctx, arguments);
+    }
+    ctx.ui->Error(BadUsage(fmt::format(T_("unknown command: {0}"), arguments[0])));
+    return 1;
+}
