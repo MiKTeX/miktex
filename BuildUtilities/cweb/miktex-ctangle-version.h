c4f4e49e1512e24320a7aa0eeda23d871ab5d1d3
Christian Schenk <cs@miktex.org>
2022-02-21 13:23:29 +0100

    refurbish: cweb

---
+++ BuildUtilities/cweb/miktex-ctangle-version.h
@@ -0,0 +1,21 @@
+/**
+ * @file miktex-ctangle-version.h
+ * @author Christian Schenk
+ * @brief Version number
+ *
+ * @copyright Copyright © 1996-2022 Christian Schenk
+ *
+ * This file is free software; the copyright holder gives unlimited permission
+ * to copy and/or distribute it, with or without modifications, as long as this
+ * notice is preserved.
+ */
+
+#define MIKTEX_COMP_MAJOR_VERSION 4
+#define MIKTEX_COMP_MINOR_VERSION 0
+#define MIKTEX_COMP_PATCH_VERSION 0
+
+#define MIKTEX_COMP_ORIG_VERSION_STR "3.64"
+
+#define MIKTEX_COMP_COPYRIGHT_STR "© 1987,1990,1993,2000 Silvio Levy and Donald E. Knuth"
+
+#include <miktex/Version>
