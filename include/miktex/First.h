dd5fd59ff812356b588e22fbff77ff52afdb1946
Christian Schenk <cs@miktex.org>
2022-02-24 11:03:15 +0100

    refurbish: include

--- include/miktex/First.h
+++ include/miktex/First.h
@@ -1,47 +1,34 @@
-/* miktex/First.h:                                      -*- C++ -*-
+/**
+ * @file miktex/First.h
+ * @author Christian Schenk
+ * @brief MiKTeX header to be included first
+ *
+ * @copyright Copyright © 1996-2022 Christian Schenk
+ *
+ * This file is free software; the copyright holder gives
+ * unlimited permission to copy and/or distribute it, with or
+ * without modifications, as long as this notice is preserved.
+ */
 
-   Copyright (C) 1996-2017 Christian Schenk
-
-   This file is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published
-   by the Free Software Foundation; either version 2, or (at your
-   option) any later version.
-
-   This file is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this file; if not, write to the Free Software
-   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-   USA. */
-
-#if defined(_MSC_VER)
 #pragma once
-#endif
-
-#if !defined(MIKTEX_FIRST_H)
-#define MIKTEX_FIRST_H
 
 #if defined(_MSC_VER)
-#include <crtdbg.h>
+#   include <crtdbg.h>
 #endif
 
 #if defined(_MSC_VER) && defined(__cplusplus)
 
-namespace MiKTeX {
-  namespace Debug {
-    void
+namespace MiKTeX
+{
+    namespace Debug {
+        void
 #if defined(EAD86981C92C904D808A5E6CEC64B90E)
-    __declspec(dllexport)
+            __declspec(dllexport)
 #else
-    __declspec(dllimport)
+            __declspec(dllimport)
 #endif
-    __cdecl OnThrowStdException();
-  }
+            __cdecl OnThrowStdException();
+    }
 }
 
 #endif /* Microsoft C++ */
-
-#endif
--- include/miktex/Helpers.h
+++ include/miktex/Helpers.h
@@ -1,29 +1,17 @@
-/* miktex/Helpers.h:                                    -*- C++ -*-
-
-   Copyright (C) 2020 Christian Schenk
-
-   This file is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published
-   by the Free Software Foundation; either version 2, or (at your
-   option) any later version.
-
-   This file is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this file; if not, write to the Free Software
-   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-   USA. */
+/**
+ * @file miktex/Helpers.h
+ * @author Christian Schenk
+ * @brief MiKTeX helper macros
+ *
+ * @copyright Copyright © 2020-2022 Christian Schenk
+ *
+ * This file is free software; the copyright holder gives
+ * unlimited permission to copy and/or distribute it, with or
+ * without modifications, as long as this notice is preserved.
+ */
 
 #pragma once
-
-#if !defined(C71F5FF9EC5948E7B2A003BFB017CEE8)
-#define C71F5FF9EC5948E7B2A003BFB017CEE8
 
 #define MIKTEX_UNIQUE__(x, y) x##y
 #define MIKTEX_UNIQUE_(x, y) MIKTEX_UNIQUE__(x, y)
 #define MIKTEX_UNIQUE(x) MIKTEX_UNIQUE_(x, __COUNTER__)
-
-#endif
