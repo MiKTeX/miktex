9ecd6393565e51f06bc140253e6ff944eddf2c97
Christian Schenk <cs@miktex.org>
2022-03-06 11:57:58 +0100

    refurbish: ptex

--- Programs/TeXAndFriends/texjp/euptex/c4p_pre.h
+++ Programs/TeXAndFriends/texjp/euptex/c4p_pre.h
@@ -3,7 +3,7 @@
  * @author Christian Schenk
  * @brief C4P first things first
  *
- * @copyright Copyright © 1921-2022 Christian Schenk
+ * @copyright Copyright © 2021-2022 Christian Schenk
  *
  * This file is free software; the copyright holder gives unlimited permission
  * to copy and/or distribute it, with or without modifications, as long as this
