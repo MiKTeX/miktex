b428405b0c528a468a0f7fd2bb7a4ca66442ba84
Christian Schenk <cs@miktex.org>
2022-03-08 12:58:22 +0100

    New MiKTeX icon

--- cmake/modules/SourcePaths.cmake
+++ cmake/modules/SourcePaths.cmake
@@ -192,9 +192,9 @@ set(MIKTEX_DOWNLOAD_ANIMATION           "${CMAKE_SOURCE_DIR}/Resources/Videos/do
 set(MIKTEX_DPIAWARE_MANIFEST            "${CMAKE_SOURCE_DIR}/Resources/Manifests/dpiAware.manifest")
 set(MIKTEX_FILECOPY_ANIMATION           "${CMAKE_SOURCE_DIR}/Resources/Videos/filecopy.avi")
 set(MIKTEX_ICNS                         "${CMAKE_SOURCE_DIR}/Resources/Graphics/miktex.icns")
-set(MIKTEX_ICON                         "${CMAKE_SOURCE_DIR}/Resources/Graphics/miktex2018.ico")
+set(MIKTEX_ICON                         "${CMAKE_SOURCE_DIR}/Resources/Graphics/miktex2022.ico")
 set(MIKTEX_PACKAGE_MANAGER_ICON         "${CMAKE_SOURCE_DIR}/Resources/Graphics/miktex-package-manager.ico")
-set(MIKTEX_WIZARD_HEADER_BITMAP         "${CMAKE_SOURCE_DIR}/Resources/Graphics/wiz97-header.bmp")
+set(MIKTEX_WIZARD_HEADER_BITMAP         "${CMAKE_SOURCE_DIR}/Resources/Graphics/wiz97-header-2022.bmp")
 set(MIKTEX_WIZARD_WATERMARK_BITMAP      "${CMAKE_SOURCE_DIR}/Resources/Graphics/wiz97-watermark.bmp")
 
 # sed scripts
