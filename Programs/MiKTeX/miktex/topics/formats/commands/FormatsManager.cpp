854a42cc906c86ba0443ee1bae086071e369944c
Christian Schenk <cs@miktex.org>
2022-03-08 10:03:00 +0100

    cosmetics

--- Programs/MiKTeX/miktex/topics/formats/commands/FormatsManager.cpp
+++ Programs/MiKTeX/miktex/topics/formats/commands/FormatsManager.cpp
@@ -43,105 +43,105 @@ void FormatsManager::Init(ApplicationContext& ctx)
 
 void FormatsManager::Build(const string& formatKey)
 {
-  if (find(this->formatsMade.begin(), this->formatsMade.end(), formatKey) != this->formatsMade.end())
-  {
-    return;
-  }
-
-  auto formatInfo = this->Format(formatKey);
-
-  this->ctx->ui->Verbose(0, fmt::format(T_("Building format '{0}' with engine '{1}'..."), formatInfo.key, formatInfo.compiler));
-
-  string maker;
-
-  vector<string> arguments;
-
-  if (formatInfo.compiler == "mf")
-  {
-    maker = MIKTEX_MAKEBASE_EXE;
-  }
-  else
-  {
-    maker = MIKTEX_MAKEFMT_EXE;
-    arguments.push_back("--engine="s + formatInfo.compiler);
-  }
-
-  arguments.push_back("--dest-name="s + formatInfo.name);
-
-  if (!formatInfo.preloaded.empty())
-  {
-    if (PathName::Compare(formatInfo.preloaded, formatKey) == 0)
+    if (find(this->formatsMade.begin(), this->formatsMade.end(), formatKey) != this->formatsMade.end())
     {
-      this->ctx->ui->FatalError(fmt::format(T_("{0}: rule recursion"), formatKey));
+        return;
     }
-    // RECURSION
-    this->Build(formatInfo.preloaded);
-    arguments.push_back("--preload="s + formatInfo.preloaded);
-  }
 
-  if (PathName(formatInfo.inputFile).HasExtension(".ini"))
-  {
-    arguments.push_back("--no-dump");
-  }
+    auto formatInfo = this->Format(formatKey);
 
-  arguments.push_back(formatInfo.inputFile);
+    this->ctx->ui->Verbose(0, fmt::format(T_("Building format '{0}' with engine '{1}'..."), formatInfo.key, formatInfo.compiler));
 
-  for (auto a : formatInfo.arguments)
-  {
-    arguments.push_back("--engine-option="s + a);
-  }
+    string maker;
 
-  this->RunMakeTeX(maker, arguments);
+    vector<string> arguments;
 
-  this->formatsMade.push_back(formatKey);
+    if (formatInfo.compiler == "mf")
+    {
+        maker = MIKTEX_MAKEBASE_EXE;
+    }
+    else
+    {
+        maker = MIKTEX_MAKEFMT_EXE;
+        arguments.push_back("--engine="s + formatInfo.compiler);
+    }
+
+    arguments.push_back("--dest-name="s + formatInfo.name);
+
+    if (!formatInfo.preloaded.empty())
+    {
+        if (PathName::Compare(formatInfo.preloaded, formatKey) == 0)
+        {
+            this->ctx->ui->FatalError(fmt::format(T_("{0}: rule recursion"), formatKey));
+        }
+        // RECURSION
+        this->Build(formatInfo.preloaded);
+        arguments.push_back("--preload="s + formatInfo.preloaded);
+    }
+
+    if (PathName(formatInfo.inputFile).HasExtension(".ini"))
+    {
+        arguments.push_back("--no-dump");
+    }
+
+    arguments.push_back(formatInfo.inputFile);
+
+    for (auto a : formatInfo.arguments)
+    {
+        arguments.push_back("--engine-option="s + a);
+    }
+
+    this->RunMakeTeX(maker, arguments);
+
+    this->formatsMade.push_back(formatKey);
 }
 
 void FormatsManager::RunMakeTeX(const string& makeProg, const vector<string>& arguments)
 {
-  PathName exe;
+    PathName exe;
 
-  if (!this->ctx->session->FindFile(makeProg, FileType::EXE, exe))
-  {
-    this->ctx->ui->FatalError(fmt::format(T_("{0}: not be found"), Q_(makeProg)));
-  }
+    if (!this->ctx->session->FindFile(makeProg, FileType::EXE, exe))
+    {
+        this->ctx->ui->FatalError(fmt::format(T_("{0}: not be found"), Q_(makeProg)));
+    }
 
-  vector<string> xArguments{ makeProg };
+    vector<string> xArguments{ makeProg };
 
-  xArguments.insert(xArguments.end(), arguments.begin(), arguments.end());
+    xArguments.insert(xArguments.end(), arguments.begin(), arguments.end());
 
-  if (ctx->ui->VerbosityLevel() > 0)
-  {
-    xArguments.push_back("--verbose");
-  }
+    if (ctx->ui->VerbosityLevel() > 0)
+    {
+        xArguments.push_back("--verbose");
+    }
 
-  if (this->ctx->ui->BeingQuiet())
-  {
-    xArguments.push_back("--quiet");
-  }
+    if (this->ctx->ui->BeingQuiet())
+    {
+        xArguments.push_back("--quiet");
+    }
 
-  if (this->ctx->session->IsAdminMode())
-  {
-    xArguments.push_back("--admin");
-  }
+    if (this->ctx->session->IsAdminMode())
+    {
+        xArguments.push_back("--admin");
+    }
 
-  if (this->ctx->installer->IsInstallerEnabled())
-  {
-    xArguments.push_back("--enable-installer");
-  }
-  else
-  {
-    xArguments.push_back("--disable-installer");
-  }
+    if (this->ctx->installer->IsInstallerEnabled())
+    {
+        xArguments.push_back("--enable-installer");
+    }
+    else
+    {
+        xArguments.push_back("--disable-installer");
+    }
 
-  xArguments.push_back("--miktex-disable-maintenance");
-  xArguments.push_back("--miktex-disable-diagnose");
+    xArguments.push_back("--miktex-disable-maintenance");
+    xArguments.push_back("--miktex-disable-diagnose");
 
-  this->ctx->processRunner->RunProcess(exe, xArguments);
+    this->ctx->processRunner->RunProcess(exe, xArguments);
 }
 
 vector<FormatInfo> FormatsManager::Formats()
 {
-  return this->ctx->session->GetFormats();
+    return this->ctx->session->GetFormats();
 }
 
 FormatInfo FormatsManager::Format(const string& formatKey)
