5f1821869e4063e6b7baae725a1a7c1b6c7484b8
Christian Schenk <cs@miktex.org>
2022-12-02 13:21:48 +0100

    tl sync

--- Programs/TeXAndFriends/pdftex/pdftex/miktex-pdftex-version.h
+++ Programs/TeXAndFriends/pdftex/pdftex/miktex-pdftex-version.h
@@ -13,7 +13,7 @@
 #include "pdftex_version.h"
 
 #define MIKTEX_COMP_MAJOR_VERSION 4
-#define MIKTEX_COMP_MINOR_VERSION 12
+#define MIKTEX_COMP_MINOR_VERSION 13
 #define MIKTEX_COMP_PATCH_VERSION 0
 
 #define MIKTEX_COMP_ID "miktex-pdftex"
