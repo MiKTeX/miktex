1ac999431d60e92c4ccf6f4100f7a47da539bf3f
Christian Schenk <cs@miktex.org>
2022-12-10 21:15:44 +0100

    refurbish

---
+++ Programs/Validation/lacheck/version.cmake
@@ -0,0 +1,15 @@
+## version.cmake
+##
+## Copyright (C) 2022 Christian Schenk
+## 
+## This file is free software; the copyright holder gives
+## unlimited permission to copy and/or distribute it, with or
+## without modifications, as long as this notice is preserved.
+
+set(MIKTEX_COMP_MAJOR_VERSION 4)
+set(MIKTEX_COMP_MINOR_VERSION 0)
+set(MIKTEX_COMP_PATCH_VERSION 0)
+
+set(MIKTEX_COMP_ORIG_VERSION_STR "1.26")
+
+set(MIKTEX_COMP_COPYRIGHT_STR "© 1991-1992 Kresten Krab Thorup, © 1993-1998 Per Abrahamsen")
