960b7b6254e550498980a865c385484ffc711336
Christian Schenk <cs@miktex.org>
2022-12-30 22:11:41 +0100

    refurbish

---
+++ Libraries/3rd/png/component.cmake
@@ -0,0 +1,13 @@
+## component.cmake
+##
+## Copyright (C) 2022 Christian Schenk
+## 
+## This file is free software; the copyright holder gives
+## unlimited permission to copy and/or distribute it, with or
+## without modifications, as long as this notice is preserved.
+
+include(version.cmake)
+
+set(MIKTEX_COMP_COMPANY_STR "")
+set(MIKTEX_COMP_COPYRIGHT_STR "© 1998-2018 Glenn Randers-Pehrson")
+set(MIKTEX_COMP_DESCRIPTION "PNG reference library")
