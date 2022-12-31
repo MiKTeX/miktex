2181535db445c1356420b645f6e05d958a3d6452
Christian Schenk <cs@miktex.org>
2022-12-31 18:36:42 +0100

    refurbish

--- Libraries/3rd/regex/component.cmake
+++ Libraries/3rd/regex/component.cmake
@@ -9,5 +9,5 @@
 include(version.cmake)
 
 set(MIKTEX_COMP_COMPANY_STR "Free Software Foundation, Inc.")
-set(MIKTEX_COMP_COPYRIGHT_STR "© 1989-2022 Free Software Foundation, Inc.")
-set(MIKTEX_COMP_DESCRIPTION "GNU getopt library")
+set(MIKTEX_COMP_COPYRIGHT_STR "© 2002, 2003 Free Software Foundation, Inc.")
+set(MIKTEX_COMP_DESCRIPTION "GNU regex library")
--- Libraries/3rd/regex/version.cmake
+++ Libraries/3rd/regex/version.cmake
@@ -1,4 +1,4 @@
-## component.cmake
+## version.cmake
 ##
 ## Copyright (C) 2022 Christian Schenk
 ## 
@@ -6,8 +6,10 @@
 ## unlimited permission to copy and/or distribute it, with or
 ## without modifications, as long as this notice is preserved.
 
-include(version.cmake)
+set(MIKTEX_COMP_ORIG_VERSION_STR "2.3.6")
 
-set(MIKTEX_COMP_COMPANY_STR "Free Software Foundation, Inc.")
-set(MIKTEX_COMP_COPYRIGHT_STR "© 1989-2022 Free Software Foundation, Inc.")
-set(MIKTEX_COMP_DESCRIPTION "GNU getopt library")
+set(MIKTEX_COMP_MAJOR_VERSION 4)
+set(MIKTEX_COMP_MINOR_VERSION 0)
+set(MIKTEX_COMP_PATCH_VERSION 0)
+
+set(MIKTEX_COMP_INTERFACE_VERSION 3)
