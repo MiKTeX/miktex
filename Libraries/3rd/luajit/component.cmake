6beb474ffa2559700808ef90e044cd710bce8b4b
Christian Schenk <cs@miktex.org>
2022-12-31 12:06:03 +0100

    refurbish

--- Libraries/3rd/luajit/component.cmake
+++ Libraries/3rd/luajit/component.cmake
@@ -1,6 +1,6 @@
 ## component.cmake
 ##
-## Copyright (C) 2017-2022 Christian Schenk
+## Copyright (C) 2022 Christian Schenk
 ## 
 ## This file is free software; the copyright holder gives
 ## unlimited permission to copy and/or distribute it, with or
@@ -9,5 +9,5 @@
 include(version.cmake)
 
 set(MIKTEX_COMP_COMPANY_STR "")
-set(MIKTEX_COMP_COPYRIGHT_STR "© 1994-2020 Lua.org, PUC-Rio")
-set(MIKTEX_COMP_DESCRIPTION "Lua scripting language library")
+set(MIKTEX_COMP_COPYRIGHT_STR "© 2005-2015 Mike Pall")
+set(MIKTEX_COMP_DESCRIPTION "LuaJIT - just-in-time compiler for Lua")
