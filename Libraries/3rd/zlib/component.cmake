b7011f73255d5a24e21aa4ffd63fd7ec16726e61
Christian Schenk <cs@miktex.org>
2023-01-01 12:52:11 +0100

    refurbish

---
+++ Libraries/3rd/zlib/component.cmake
@@ -0,0 +1,13 @@
+## component.cmake
+##
+## Copyright (C) 2023 Christian Schenk
+## 
+## This file is free software; the copyright holder gives
+## unlimited permission to copy and/or distribute it, with or
+## without modifications, as long as this notice is preserved.
+
+include(version.cmake)
+
+set(MIKTEX_COMP_COMPANY_STR "")
+set(MIKTEX_COMP_COPYRIGHT_STR "© 1995-2017 Jean-loup Gailly and Mark Adler")
+set(MIKTEX_COMP_DESCRIPTION "zlib compression library")
