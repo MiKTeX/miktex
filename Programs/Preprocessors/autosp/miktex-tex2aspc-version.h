e6075e349eabdd8d9e46d60f482fa1cea1d44d7b
Christian Schenk <cs@miktex.org>
2023-10-10 11:29:23 +0200

    refurbish

---
+++ Programs/Preprocessors/autosp/miktex-tex2aspc-version.h
@@ -0,0 +1,21 @@
+/**
+ * @file miktex-tex2aspc-version.h
+ * @author Christian Schenk
+ * @brief Version number
+ *
+ * @copyright Copyright © 2017-2023 Christian Schenk
+ *
+ * This file is free software; the copyright holder gives unlimited permission
+ * to copy and/or distribute it, with or without modifications, as long as this
+ * notice is preserved.
+ */
+
+#define MIKTEX_COMP_MAJOR_VERSION 4
+#define MIKTEX_COMP_MINOR_VERSION 0
+#define MIKTEX_COMP_PATCH_VERSION 0
+
+#define MIKTEX_COMP_ORIG_VERSION_STR "2019-01-08"
+
+#define MIKTEX_COMP_COPYRIGHT_STR "© 2017-19 R. D. Tennent School of Computing, Queen's University"
+
+#include <miktex/Version>
